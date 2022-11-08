e1169956ebdd096112cebfd252aeb399b464a521
husandro <42209726+husandro@users.noreply.github.com>
2022-11-08 17:01:59 +0100

    systeminfo

--- GamePlus/SystemInfo.lua
+++ GamePlus/SystemInfo.lua
@@ -3,59 +3,12 @@ local addName=SYSTEM_MESSAGES--MAINMENU_BUTTON
 local Save={equipmetLevel=true}
 
 local panel=CreateFrame('Button', nil, UIParent)
-panel:SetSize(12,12)
-panel:SetHighlightAtlas(e.Icon.highlight)
-panel:SetPushedAtlas(e.Icon.pushed)
-panel:SetFrameStrata('HIGH')
-
-panel.fpsms=e.Cstr(panel, Save.size)--fpsms
-panel.fpsms:SetPoint('BOTTOMRIGHT')
-
-panel.money=e.Cstr(panel, Save.size)--钱
-panel.money:SetPoint('BOTTOMRIGHT', panel.fpsms, 'BOTTOMLEFT', -4, 0)
-
-panel.durabiliy=e.Cstr(panel,Save.size)--耐久度
-panel.durabiliy:SetPoint('BOTTOMRIGHT', panel.money, 'BOTTOMLEFT', -4, 0)
 
 local function setStrColor()
     e.Cstr(nil,Save.size, nil ,panel.fpsms, true)
     e.Cstr(nil,Save.size, nil ,panel.money, true)
     e.Cstr(nil,Save.size, nil ,panel.durabiliy, true)
 end
-local function setInit()
-    if Save.point then
-        panel:SetPoint(Save.point[1], UIParent, Save.point[3], Save.point[4], Save.point[5])
-    else
-        panel:SetPoint('BOTTOMRIGHT',-24, 0)
-    end
-    setStrColor()
-end
-
-panel:SetMovable(true)
-panel:RegisterForDrag("RightButton");
-panel:SetClampedToScreen(true);
-panel:SetScript("OnDragStart", function(self2, d)
-    if d=='RightButton' and not IsModifierKeyDown() then
-        SetCursor('UI_MOVE_CURSOR')
-        self2:StartMoving()
-    end
-end)
-panel:SetScript("OnDragStop", function(self2)
-    self2:StopMovingOrSizing()
-    Save.point={self2:GetPoint(1)}
-    print(id, addName, '|cFF00FF00Alt+'..e.Icon.right..KEY_BUTTON2..'|r: '.. TRANSMOGRIFY_TOOLTIP_REVERT)
-    ResetCursor()
-    setStrColor()
-end)
-panel:SetScript("OnMouseUp", function(self2,d)
-    if d=='RightButton' and IsAltKeyDown() then
-        self2:ClearAllPoints();
-        self2:SetPoint('BOTTOMRIGHT',-24, 0)
-        Save.point=nil
-        setStrColor()
-    end
-    ResetCursor()
-end)
 
 local function setMoney()
     if not Save.money then
@@ -104,37 +57,6 @@ local function setDurabiliy(re)
     end
 end
 
-local timeElapsed = 0
-panel:HookScript("OnUpdate", function (self, elapsed)--fpsms
-    if Save.disabled then
-        return
-    end
-    timeElapsed = timeElapsed + elapsed        
-    if timeElapsed > 0.5 then
-        timeElapsed = 0
-        local t = select(4, GetNetStats())--ms
-        if t>400 then 
-            t='|cnRED_FONT_COLOR:'..t..'|r'
-        elseif t>120 then
-            t='|cnYELLOW_FONT_COLOR:'..t..'|r'
-        --else
-          --  t='|cnGREEN_FONT_COLOR:'..t..'|r'
-        end
-
-        local r=math.modf(GetFramerate())--fps
-        if r<10 then
-            r='|cff00ff00'..r..'|r'
-        elseif r<20 then
-            r='|cffffff00'..r..'|r'
-        --elseif r>30 then
-          --  r='|cff00ff00'..r..'|r'
-        end
-        t=t..'ms  '..r..'fps'
-
-        panel.fpsms:SetText(t)
-    end
-end)
-
 local function setEquipmentLevel()--角色图标显示装等
     local to, cu=GetAverageItemLevel()
     local text
@@ -153,94 +75,200 @@ local function setEquipmentLevel()--角色图标显示装等
     end
 end
 
-panel:SetScript('OnMouseWheel',function(self, d)
-    if IsModifierKeyDown() then
-        return
+local timeElapsed = 0
+local function setInit()
+    if Save.point then
+        panel:SetPoint(Save.point[1], UIParent, Save.point[3], Save.point[4], Save.point[5])
+    else
+        panel:SetPoint('BOTTOMRIGHT',-24, 0)
     end
-    local size=Save.size or 12
-    if d==1 then
-        size=size+1
-        size = size>72 and 72 or size
-    elseif d==-1 then
-        size=size-1
-        size= size<6 and 6 or size
-    end
-    Save.size=size
-    setStrColor()
-    print(id, addName, FONT_SIZE..': '..size)
-end)
-panel:SetScript('OnMouseDown', function(self, d)
-    local key=IsModifierKeyDown()
-    if d=='LeftButton' and not key then--fpsms
-        if Save.disabled then
-            Save.disabled=nil
-        else
-            Save.disabled=true
-            panel.fpsms:SetText('')
-        end
-        print(id, addName, 'FpsMs', e.GetShowHide(not Save.disabled))
-    elseif d=='LeftButton' and IsAltKeyDown() then--money
-        if Save.money then
-            Save.money=nil
-            panel.money:SetText('')
-        else
-            Save.money=true
-            setMoney()
-        end
-        print(id, addName, MONEY, e.GetShowHide(Save.money))
-    elseif d=='LeftButton' and IsControlKeyDown() then--耐久度
-        if Save.durabiliy then
-            Save.durabiliy=nil
-            panel.durabiliy:SetText("")
-        else
-            Save.durabiliy=true
-            setDurabiliy()
-        end
-        print(id, addName, DURABILITY, e.GetShowHide(Save.durabiliy))
-    elseif d=='LeftButton' and IsShiftKeyDown() then--角色图标显示装等
-        if Save.equipmetLevel then
-            Save.equipmetLevel=nil
-        else
-            Save.equipmetLevel=true
-        end
-        setEquipmentLevel()
-        print(id,addName, EQUIPSET_EQUIP..LEVEL, e.GetShowHide(Save.equipmetLevel))
-    end
-end)
 
-panel:SetScript('OnEnter', function()
-    if UnitAffectingCombat('player') then
-        return
-    end
-    e.tips:SetOwner(panel.money, "ANCHOR_LEFT",0, 30);
-    e.tips:ClearLines();
-    e.tips:AddDoubleLine(id, addName)
-    e.tips:AddLine(' ')
-    e.tips:AddDoubleLine('FpsMs: '..e.GetShowHide(not Save.disabled), e.Icon.left)
-    e.tips:AddDoubleLine(MONEY..': '..e.GetShowHide(Save.money), 'Alt+'..e.Icon.left)
-    local du=setDurabiliy(true)
-    du = du and ' '..du or ''
-    e.tips:AddDoubleLine(DURABILITY..du..': '..e.GetShowHide(Save.durabiliy), 'Ctrl+'..e.Icon.left)
-    e.tips:AddDoubleLine(EQUIPSET_EQUIP..LEVEL..': '..e.GetShowHide(Save.equipmetLevel), 'Shift+'..e.Icon.left)
-    e.tips:AddLine(' ')
-    e.tips:AddDoubleLine(UI_SCALE..': '..(Save.size or 12), e.Icon.mid)
-    e.tips:AddDoubleLine(NPE_MOVE,e.Icon.right)
-    e.tips:Show();
-end)
-panel:SetScript('OnLeave', function() e.tips:Hide() end)
+    panel:SetSize(12,12)
+    panel:SetHighlightAtlas(e.Icon.highlight)
+    panel:SetPushedAtlas(e.Icon.pushed)
+    panel:SetFrameStrata('HIGH')
+
+    panel.fpsms=e.Cstr(panel, Save.size)--fpsms
+    panel.fpsms:SetPoint('BOTTOMRIGHT')
+
+    panel.money=e.Cstr(panel, Save.size)--钱
+    panel.money:SetPoint('BOTTOMRIGHT', panel.fpsms, 'BOTTOMLEFT', -4, 0)
+
+    panel.durabiliy=e.Cstr(panel,Save.size)--耐久度
+    panel.durabiliy:SetPoint('BOTTOMRIGHT', panel.money, 'BOTTOMLEFT', -4, 0)
+
+    panel:SetMovable(true)
+    panel:RegisterForDrag("RightButton");
+    panel:SetClampedToScreen(true);
+    panel:SetScript("OnDragStart", function(self2, d)
+        if d=='RightButton' and not IsModifierKeyDown() then
+            SetCursor('UI_MOVE_CURSOR')
+            self2:StartMoving()
+        end
+    end)
+    panel:SetScript("OnDragStop", function(self2)
+        self2:StopMovingOrSizing()
+        Save.point={self2:GetPoint(1)}
+        print(id, addName, '|cFF00FF00Alt+'..e.Icon.right..KEY_BUTTON2..'|r: '.. TRANSMOGRIFY_TOOLTIP_REVERT)
+        ResetCursor()
+        setStrColor()
+    end)
+    panel:SetScript("OnMouseUp", function(self2,d)
+        if d=='RightButton' and IsAltKeyDown() then
+            self2:ClearAllPoints();
+            self2:SetPoint('BOTTOMRIGHT',-24, 0)
+            Save.point=nil
+            setStrColor()
+        end
+        ResetCursor()
+    end)
+
+    panel.fpsmsFrame=CreateFrame("Frame",nil,panel)
+    panel.fpsmsFrame:SetPoint('RIGHT')
+    panel.fpsmsFrame:SetSize(1,1)
+    panel.fpsmsFrame:HookScript("OnUpdate", function (self, elapsed)--fpsms
+        timeElapsed = timeElapsed + elapsed        
+        if timeElapsed > 0.5 then
+            timeElapsed = 0
+            local t = select(4, GetNetStats())--ms
+            if t>400 then 
+                t='|cnRED_FONT_COLOR:'..t..'|r'
+            elseif t>120 then
+                t='|cnYELLOW_FONT_COLOR:'..t..'|r'
+            end
+            local r=math.modf(GetFramerate())--fps
+            if r then
+                if r<10 then
+                    r='|cff00ff00'..r..'|r'
+                elseif r<20 then
+                    r='|cffffff00'..r..'|r'
+                end
+                t=t..'ms  '..r..'fps'
+            end
+            panel.fpsms:SetText(t)
+        end
+    end)
+
+    panel:SetScript('OnMouseWheel',function(self, d)
+        if IsModifierKeyDown() then
+            return
+        end
+        local size=Save.size or 12
+        if d==1 then
+            size=size+1
+            size = size>72 and 72 or size
+        elseif d==-1 then
+            size=size-1
+            size= size<6 and 6 or size
+        end
+        Save.size=size
+        setStrColor()
+        print(id, addName, FONT_SIZE..': '..size)
+    end)
+
+    panel:SetScript('OnMouseDown', function(self, d)
+        local key=IsModifierKeyDown()
+        if d=='LeftButton' and not key then--fpsms
+            if Save.hideFpsMs then
+                Save.hideFpsMs=nil
+                timeElapsed=0
+            else
+                Save.hideFpsMs=true
+                panel.fpsms:SetText('')
+            end
+            panel.fpsmsFrame:SetShown(not Save.hideFpsMs)
+            print(id, addName, 'FpsMs', e.GetShowHide(not Save.hideFpsMs))
+
+        elseif d=='LeftButton' and IsAltKeyDown() then--money
+            if Save.money then
+                Save.money=nil
+                panel.money:SetText('')
+            else
+                Save.money=true
+                setMoney()
+            end
+            print(id, addName, MONEY, e.GetShowHide(Save.money))
+        elseif d=='LeftButton' and IsControlKeyDown() then--耐久度
+            if Save.durabiliy then
+                Save.durabiliy=nil
+                panel.durabiliy:SetText("")
+            else
+                Save.durabiliy=true
+                setDurabiliy()
+            end
+            print(id, addName, DURABILITY, e.GetShowHide(Save.durabiliy))
+        elseif d=='LeftButton' and IsShiftKeyDown() then--角色图标显示装等
+            if Save.equipmetLevel then
+                Save.equipmetLevel=nil
+            else
+                Save.equipmetLevel=true
+            end
+            setEquipmentLevel()
+            print(id,addName, EQUIPSET_EQUIP..LEVEL, e.GetShowHide(Save.equipmetLevel))
+        end
+    end)
+    panel:SetScript('OnEnter', function()
+        if UnitAffectingCombat('player') then
+            return
+        end
+        e.tips:SetOwner(panel.money, "ANCHOR_LEFT",0, 30);
+        e.tips:ClearLines();
+        e.tips:AddDoubleLine(id, addName)
+        e.tips:AddLine(' ')
+        e.tips:AddDoubleLine('FpsMs: '..e.GetShowHide(not Save.disabled), e.Icon.left)
+        e.tips:AddDoubleLine(MONEY..': '..e.GetShowHide(Save.money), 'Alt+'..e.Icon.left)
+        local du=setDurabiliy(true)
+        du = du and ' '..du or ''
+        e.tips:AddDoubleLine(DURABILITY..du..': '..e.GetShowHide(Save.durabiliy), 'Ctrl+'..e.Icon.left)
+        e.tips:AddDoubleLine(EQUIPSET_EQUIP..LEVEL..': '..e.GetShowHide(Save.equipmetLevel), 'Shift+'..e.Icon.left)
+        e.tips:AddLine(' ')
+        e.tips:AddDoubleLine(UI_SCALE..': '..(Save.size or 12), e.Icon.mid)
+        e.tips:AddDoubleLine(NPE_MOVE,e.Icon.right)
+        e.tips:Show();
+    end)
+    panel:SetScript('OnLeave', function() e.tips:Hide() end)
+
+    setStrColor()
+    setMoney()
+    setDurabiliy()
+   C_Timer.After(2, setEquipmentLevel) --角色图标显示装等   
+end
 
 panel:RegisterEvent("ADDON_LOADED")
-panel:RegisterEvent("PLAYER_LOGOUT")
+
 panel:RegisterEvent("PLAYER_MONEY")
 panel:RegisterEvent("UPDATE_INVENTORY_DURABILITY")
 panel:RegisterEvent('PLAYER_EQUIPMENT_CHANGED')
+
 panel:SetScript("OnEvent", function(self, event, arg1)
     if event == "ADDON_LOADED" and arg1==id then
         Save= WoWToolsSave and WoWToolsSave[addName] or Save
-        setInit()
-        setMoney()
-        setDurabiliy()
-       C_Timer.After(2, setEquipmentLevel) --角色图标显示装等        
+
+        local check=e.CPanel(addName, not Save.disabled, true)
+        check:SetScript('OnClick', function()
+            if Save.disabled then
+                Save.disabled=nil
+            else
+                Save.disabled=true
+            end
+            print(id, addName, e.GetEnabeleDisable(not Save.disabled), REQUIRES_RELOAD, '/reload')
+        end)
+        check:SetScript('OnEnter', function(self2)
+            e.tips:SetOwner(self2, "ANCHOR_RIGHT");
+            e.tips:ClearLines();
+            e.tips:AddDoubleLine('fps ms', MONEY)
+            e.tips:AddDoubleLine(DURABILITY, EQUIPSET_EQUIP..LEVEL)
+            e.tips:Show();
+        end)
+        check:SetScript('OnLeave', function() e.tips:Hide() end)
+
+        if Save.disabled then
+            panel:UnregisterAllEvents()
+        else
+            setInit()
+        end
+        panel:RegisterEvent("PLAYER_LOGOUT")
+
     elseif event == "PLAYER_LOGOUT" then
         if not e.ClearAllSave then
             if not WoWToolsSave then WoWToolsSave={} end
