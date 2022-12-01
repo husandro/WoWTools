e54cecb2cb530f11b939a48fbab74df1d940cbbb
husandro <42209726+husandro@users.noreply.github.com>
2022-12-01 16:33:24 +0100

    Update Markers.lua

--- ChatButton/Markers.lua
+++ ChatButton/Markers.lua
@@ -1,6 +1,6 @@
 local id, e = ...
 local addName= BINDING_HEADER_RAID_TARGET
-local Save={ autoSet=true, tank=2, tank2=6, healer=1, countdown=7, groupReadyTips=true, markersScale=0.85}
+local Save={ autoSet=true, tank=2, tank2=6, healer=1, countdown=7, groupReadyTips=true, markersScale=0.85, markersFrame=true}
 
 local panel=e.Cbtn2(nil, WoWToolsChatButtonFrame, true, false)
 panel:SetPoint('LEFT',WoWToolsChatButtonFrame.last, 'RIGHT')--设置位置
