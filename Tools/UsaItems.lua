7944260ba2aab8bf71cbec6b4c66bc9aa8611371
husandro <husandro@qq.com>
2022-12-03 21:49:43 +0000

    ...

--- Tools/UsaItems.lua
+++ Tools/UsaItems.lua
@@ -538,15 +538,15 @@ panel:SetScript("OnEvent", function(self, event, arg1)
         Save= WoWToolsSave and WoWToolsSave[addName..'Tools'] or Save
         if not e.toolsFrame.disabled then
 
-for _, ID in pairs(Save.item) do
-    if not C_Item.IsItemDataCachedByID(ID) then C_Item.RequestLoadItemDataByID(ID) end
-end
-for _, ID in pairs(Save.spell) do
-    if not C_Spell.IsSpellDataCached(ID) then C_Spell.RequestLoadSpellData(ID) end
-end
-for _, ID in pairs(Save.equip) do
-    if not C_Item.IsItemDataCachedByID(ID) then C_Item.RequestLoadItemDataByID(ID) end
-end
+        for _, ID in pairs(Save.item) do
+            if not C_Item.IsItemDataCachedByID(ID) then C_Item.RequestLoadItemDataByID(ID) end
+        end
+        for _, ID in pairs(Save.spell) do
+            if not C_Spell.IsSpellDataCached(ID) then C_Spell.RequestLoadSpellData(ID) end
+        end
+        for _, ID in pairs(Save.equip) do
+            if not C_Item.IsItemDataCachedByID(ID) then C_Item.RequestLoadItemDataByID(ID) end
+        end
 
             C_Timer.After(1.6, function()
                 if UnitAffectingCombat('player') then
