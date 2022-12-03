f6401ded0474b5560281c25437684a5d7d464d9c
husandro <husandro@qq.com>
2022-12-03 23:55:27 +0000

    ...

--- Tools/Food.lua
+++ Tools/Food.lua
@@ -7,18 +7,52 @@ panel:SetAttribute("type", "item")
 panel.itemID=5512--治疗石
 if not C_Item.IsItemDataCachedByID(panel.itemID) then C_Item.RequestLoadItemDataByID(panel.itemID) end
 
+--#######
+--图标冷却
+--#######
+local function set_Cooldown(self)--图标冷却
+    if self.itemID then
+        local start, duration = GetItemCooldown(self.itemID)
+        e.Ccool(self, start, duration, nil, true, nil, true)--冷却条
+    end
+end
 
-local function set_Enter(self)
+local function set_Item_Count(self)
+    self.count:SetText(GetItemCount(self.itemID))
+    self.texture:SetDesaturated(self.count==0)
+end
+
+--#########
+--提示, 事件
+--#########
+local function set_Button_Init(self)
     self:SetScript("OnEnter",function(self2)
-        if self2.itemID then
-            e.tips:SetOwner(self2, "ANCHOR_LEFT")
-            e.tips:ClearLines()
-            e.tips:SetItemByID(self2.itemID)
+        e.tips:SetOwner(self2, "ANCHOR_LEFT")
+        e.tips:ClearLines()
+        e.tips:SetItemByID(self2.itemID)
+        if self==panel then
             e.tips:AddLine(' ')
-            e.tips:Show()
+            e.tips:AddDoubleLine(MAINMENU or SLASH_TEXTTOSPEECH_MENU, e.Icon.right)
         end
+        e.tips:Show()
     end)
-    panel:SetScript("OnLeave",function() e.tips:Hide() end)
+    self:SetScript("OnLeave",function() e.tips:Hide() end)
+
+    self.count= e.Cstr(self,nil, nil,nil, true)
+    self:RegisterEvent('BAG_UPDATE_DELAYED')
+    self:RegisterEvent('BAG_UPDATE_COOLDOWN')
+    if self~=panel then
+        panel:SetScript("OnEvent", function(self2, event)
+           if event=='BAG_UPDATE_DELAYED' then
+                set_Item_Count(self2)
+            elseif event=='BAG_UPDATE_COOLDOWN' then
+                set_Cooldown(self2)--图标冷却
+            end
+        end)
+    end
+
+    set_Item_Count(self2)
+    set_Cooldown(self)--图标冷却
 end
 
 --#####
@@ -40,7 +74,8 @@ local function Init()
     local size=e.toolsFrame.size or 30
     panel:SetSize(size,size)
     panel:SetAttribute("item", C_Item.GetItemNameByID(panel.itemID))
-    set_Enter(panel)
+    set_Button_Init(panel)--提示, 事件
+    
 
     panel.Menu=CreateFrame("Frame",nil, panel, "UIDropDownMenuTemplate")
     UIDropDownMenu_Initialize(panel.Menu, InitMenu, 'MENU')
@@ -60,7 +95,6 @@ local function Init()
     panel:SetScript("OnMouseDown", function(self,d)
         ToggleDropDownMenu(1,nil,self.Menu, self, 15,0)
     end)
-
     panel:SetScript("OnMouseUp", function(self, d)
         ResetCursor()
     end)
@@ -88,5 +122,9 @@ panel:SetScript("OnEvent", function(self, event, arg1)
         end
 
     elseif event=='BAG_UPDATE_DELAYED' then
+        set_Item_Count(self)
+
+    elseif event=='BAG_UPDATE_COOLDOWN' then
+        set_Cooldown(self)--图标冷却
     end
 end)
