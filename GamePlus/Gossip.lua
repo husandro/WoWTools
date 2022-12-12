69536742e8a2f4186aea444b852f4171997b819b
husandro <42209726+husandro@users.noreply.github.com>
2022-12-12 13:59:23 +0100

    Update Gossip.lua

--- GamePlus/Gossip.lua
+++ GamePlus/Gossip.lua
@@ -57,8 +57,10 @@ local function select_Reward()--自动:选择奖励
         local  itemLink = GetQuestItemLink('choice', i)
         if itemLink then
             local amount = select(3, GetQuestItemInfo('choice', i))--钱
-            local _, _, itemQuality, itemLevel, _, _,_,_, itemEquipLoc, _, sellPrice = GetItemInfo(itemLink)
-
+            local _, _, itemQuality, itemLevel, _, _,_,_, itemEquipLoc, _, sellPrice,classID, subclassID = GetItemInfo(itemLink)
+            if classID==19 or (classID==4 and subclassID==5) or itemLevel==1 then
+                return
+            end
             if itemQuality and itemQuality<4 and IsEquippableItem(itemLink) then--最高 稀有的 3                                
                 local invSlot = itemEquipLoc and  e.itemSlotTable[itemEquipLoc]
                 if invSlot and itemLevel and itemLevel>1 then--装等
