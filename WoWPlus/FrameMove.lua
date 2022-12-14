53444fe8502eab53fa4a0b4a63ae073ea1a96e63
husandro <husandro@qq.com>
2022-12-14 18:57:25 +0100

    Update FrameMove.lua

--- WoWPlus/FrameMove.lua
+++ WoWPlus/FrameMove.lua
@@ -272,7 +272,6 @@ local function setAddLoad(arg1)
 
     elseif arg1=='Blizzard_GenericTraitUI' then--欲龙术
         Move(GenericTraitFrame,{})
-
         Move(GenericTraitFrame.ButtonsParent,{frame=GenericTraitFrame})
         
     end
