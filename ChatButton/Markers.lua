aa244781103ab1385550a8eb666478236c306aae
husandro <42209726+husandro@users.noreply.github.com>
2022-12-20 09:27:57 +0100

    Update Markers.lua

--- ChatButton/Markers.lua
+++ ChatButton/Markers.lua
@@ -327,7 +327,7 @@ local function setMarkersFrame_Postion()--设置标记框架, 位置
     if frame then
         if Save.markersFramePoint then
             frame:SetPoint(Save.markersFramePoint[1], UIParent, Save.markersFramePoint[3], Save.markersFramePoint[4], Save.markersFramePoint[5])
-        elseif MultiBarBottomLeftButton12 and MultiBarBottomLeftButton12:IsVisible() then
+        elseif MultiBarBottomLeftButton12 and MultiBarBottomLeftButton12:IsShown() then
             frame:SetPoint('BOTTOMLEFT', MultiBarBottomLeftButton12, 'TOPRIGHT')
         else
             frame:SetPoint('TOPRIGHT', panel, 'BOTTOMRIGHT', 150,100)
