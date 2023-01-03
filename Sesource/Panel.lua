118edbc128f999020baa36d54634c3eb3344920a
husandro <husandro@qq.com>
2023-01-03 21:57:10 +0100

    Update Panel.lua

--- Sesource/Panel.lua
+++ Sesource/Panel.lua
@@ -43,8 +43,8 @@ local gamePlus=e.Cstr(panel)
 gamePlus:SetPoint('TOPLEFT', panel,'TOP', 0, -14)
 gamePlus:SetText('Game Plus')
 
-local lastWoW=reloadButton
-local lastGame=gamePlus
+local lastWoW= reloadButton
+local lastGame= gamePlus
 --添加控制面板
 e.CPanel= function(name, value, GamePlus)
     local check=CreateFrame("CheckButton", nil, panel, "InterfaceOptionsCheckButtonTemplate")
@@ -76,12 +76,13 @@ panel:SetScript("OnEvent", function(self, event, arg1)
         check:SetScript('OnMouseDown',function()
             e.onlyChinse= not e.onlyChinse and true or nil
             Save.onlyChinse = e.onlyChinse
-            print(id, addName, e.GetEnabeleDisable(e.onlyChinse), e.onlyChinse and '需要重新加载' or REQUIRES_RELOAD)
+            print(id, addName, e.GetEnabeleDisable(e.onlyChinse), '|cffff00ff', e.onlyChinse and '需要重新加载' or REQUIRES_RELOAD)
         end)
-        check:SetScript('OnEnter', function(self)
-            e.tips:SetOwner(self, "ANCHOR_LEFT")
+        check:SetScript('OnEnter', function(self2)
+            e.tips:SetOwner(self2, "ANCHOR_LEFT")
             e.tips:ClearLines()
             e.tips:AddDoubleLine(LANGUAGE..'('..SHOW..')', LFG_LIST_LANGUAGE_ZHCN)
+            e.tips:AddDoubleLine('显示语言', '简体中文')
             e.tips:Show()
         end)
         check:SetScript('OnLeave', function() e.tips:Hide() end)
