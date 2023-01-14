93e1c22ac685e481c988f83e6a70e23fb06b5595
husandro <husandro@qq.com>
2023-01-15 00:41:18 +0100

    Update EncounterJournal.lua

--- WoWPlus/EncounterJournal.lua
+++ WoWPlus/EncounterJournal.lua
@@ -104,7 +104,7 @@ local function EncounterJournal_Set_All_Info_Text()--冒险指南,右边,显示�
         if head then
             text = text~='' and text..'\n' or text
             text = text..'|T450908:0|t'..head
-            if head==MYTHIC_DUNGEONS then
+            if head==MYTHIC_DUNGEONS and e.WoWSave[e.Player.guid].Keystones then
                 local weekLevel=e.WoWSave[e.Player.guid].Keystones.weekLevel--本周最高
                 if weekLevel then
                     text=text..' |cnGREEN_FONT_COLOR:'..weekLevel..'|r'
