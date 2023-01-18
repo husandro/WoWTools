5ceee6f3adcbb32f9880b1712c7eb41408405ef8
husandro <husandro@qq.com>
2023-01-18 18:40:39 +0100

    Update Tooltips.lua

--- WoWPlus/Tooltips.lua
+++ WoWPlus/Tooltips.lua
@@ -1,6 +1,6 @@
 local id, e = ...
 local addName= 'Tootips'
-local Save={setDefaultAnchor=true, setCVar=e.Player.husandro}--inCombatDefaultAnchor=true
+local Save={setDefaultAnchor=true, setCVar=e.Player.husandro,inCombatDefaultAnchor=true}
 local panel=CreateFrame("Frame")
 
 local function setInitItem(self, hide)--创建物品
