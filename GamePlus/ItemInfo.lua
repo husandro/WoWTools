70046a5f3d524cae0e5c1014690c29a6615ec8b8
husandro <husandro@qq.com>
2023-02-05 12:51:58 +0100

    Update ItemInfo.lua

--- GamePlus/ItemInfo.lua
+++ GamePlus/ItemInfo.lua
@@ -425,7 +425,7 @@ local function Init()
                         if not button.quantityAll then
                             --e.Cstr=function(self,size, fontType, ChangeFont, color, layer, justifyH)
                             button.quantityAll= e.Cstr(button, 10, nil, nil, nil, nil, 'RIGHT')
-                            button.quantityAll:SetPoint('BOTTOMRIGHT', button, 'TOPRIGHT', 0,2)
+                            button.quantityAll:SetPoint('BOTTOMRIGHT', button, 'TOPRIGHT', 3,0)
                         end
                         button.quantityAll:SetText(num or '');
                     end
