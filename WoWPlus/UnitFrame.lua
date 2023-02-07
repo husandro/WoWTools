c96e72b03beecad5169ecef9c49c1e20a7e20efa
husandro <husandro@qq.com>
2023-02-07 22:41:07 +0100

    Update UnitFrame.lua

--- WoWPlus/UnitFrame.lua
+++ WoWPlus/UnitFrame.lua
@@ -261,6 +261,8 @@ local function set_UnitFrame_Update()--职业, 图标， 颜色
                         targetFrameContentContextual.GuideIcon:SetPoint('RIGHT', self.classTexture,'LEFT')
                     end
                 end
+            elseif unit=='pet' then
+                self.classTexture:SetPoint('LEFT', self.name,-10,0)
             else
                 self.classTexture:SetPoint('TOPLEFT', self.portrait, 'TOPRIGHT',-14,10)
             end
