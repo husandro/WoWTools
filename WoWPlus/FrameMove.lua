bbdc56278a477b966050352a7c19f6f42aff680e
husandro <husandro@qq.com>
2023-02-10 15:44:04 +0100

    Update FrameMove.lua

--- WoWPlus/FrameMove.lua
+++ WoWPlus/FrameMove.lua
@@ -413,10 +413,11 @@ tex:SetAtlas('!perks-list-side-vertical')
 
     Move(MailFrame.TitleContainer,{frame=MailFrame})
 
-    if UIWidgetPowerBarContainerFrame then
+    if UIWidgetPowerBarContainerFrame then--能量条
         --e.Cbtn= function(self, Template, value, SecureAction, name, notTexture, size)
         UIWidgetPowerBarContainerFrame.moveButton= e.Cbtn(UIWidgetPowerBarContainerFrame, nil, nil, nil, nil, true, {25,25})
         UIWidgetPowerBarContainerFrame.moveButton:SetPoint('TOPRIGHT')
+        UIWidgetPowerBarContainerFrame.moveButton:SetFrameStrata('HIGH')
         Move(UIWidgetPowerBarContainerFrame.moveButton, {frame= UIWidgetPowerBarContainerFrame})
     end
 end
