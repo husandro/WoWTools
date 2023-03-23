b51647e67b782166b3c70fdbb7ff71bc312b9259
husandro <husandro@qq.com>
2023-03-23 18:39:18 +0100

    Update PaperDoll.lua

--- WoWPlus/PaperDoll.lua
+++ WoWPlus/PaperDoll.lua
@@ -944,7 +944,8 @@ local function Init()
         if ( effectiveLevel ~= level ) then
             level = EFFECTIVE_LEVEL_FORMAT:format('|cnGREEN_FONT_COLOR:'..effectiveLevel..'|r', level);
         end
-        CharacterLevelText:SetText('  |A:'..race..':26:26|a|A:'..class..':26:26|a  '..level)
+        local faction= e.Player.faction=='Alliance' and '|A:charcreatetest-logo-alliance:26:26|a' or e.Player.faction=='Horde' and '|A:charcreatetest-logo-horde:26:26|a' or ''
+        CharacterLevelText:SetText('  '..faction..'|A:'..race..':26:26|a|A:'..class..':26:26|a  '..level)
     end)
 end
 
