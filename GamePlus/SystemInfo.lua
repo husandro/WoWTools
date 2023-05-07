0f8520dbde46c8db6770703ce4837f42d6394c0b
husandro <husandro@qq.com>
2023-05-07 16:59:52 +0200

    .

--- GamePlus/SystemInfo.lua
+++ GamePlus/SystemInfo.lua
@@ -24,44 +24,64 @@ local function set_Label_Size_Color()
         e.Cstr(nil, {size=Save.size, changeFont=label, color=true})--Save.size, nil , Labels.fpsms, true)    
     end
 end
-local function create_lable(self, text)
-    local label= e.Cstr(self, {size=Save.size, color=true})--耐久度    
-    local down
-    if text=='fps' then
-        label.tooltip= function() return format(FPS_FORMATL, math.modf(GetFramerate())) end
-    elseif text=='ms' then
-        label.tooltip= function() return format(e.onlyChinese and  "延迟：\n%.0f ms （本地）\n%.0f ms （世界）" or MAINMENUBAR_LATENCY_LABEL, select(3, GetNetStats())) end
-    elseif text=='money' then
-        label.tooltip= e.onlyChinese and '钱' or MONEY
+local function create_Set_lable(self, text)--建立,或设置,Labels
+    local label= Labels[text] or e.Cstr(self, {size=Save.size, color=true})--耐久度    
+    if Save.parent then
+        local down
+        if text=='fps' then
+            label.tooltip= 'FPS'
+            down= function() securecallfunction(InterfaceOptionsFrame_OpenToCategory, id) end
+        elseif text=='ms' then
+            label.tooltip= function() return format(e.onlyChinese and  "延迟：\n%.0f ms （本地）\n%.0f ms （世界）" or MAINMENUBAR_LATENCY_LABEL, select(3, GetNetStats())) end
+            down= function() securecallfunction(InterfaceOptionsFrame_OpenToCategory, id) end
 
-    elseif text=='perksPoints' then
-        label.tooltip= e.onlyChinese and '在旅行者日志里查看赢取标币的方法' or TUTORIAL_PERKS_PROGRAM_ACTIVITIES_OPEN
-    elseif text=='durabiliy' then
-        label.tooltip= e.onlyChinese and '耐久度' or DURABILITY
-    elseif text=='equipmentLevel' then
-        label.tooltip= e.onlyChinese and '物品等级' or STAT_AVERAGE_ITEM_LEVEL 
-    end
+        elseif text=='money' then
+            label.tooltip= e.onlyChinese and '钱' or MONEY
+            down= ToggleAllBags
 
-    label:EnableMouse(true)
-    label:SetScript('OnEnter', function(self2)
-        e.tips:SetOwner(self2, "ANCHOR_LEFT")
-        e.tips:ClearLines()
-        if self2.tooltip then
-            
-            e.tips:AddLine(type(self2.tooltip)=='function' and self2.tooltip() or self2.tooltip)
-            print(self2.tooltip)
+        elseif text=='perksPoints' then
+            label.tooltip= function() 
+                local info=C_CurrencyInfo.GetCurrencyInfo(2032)
+                local str=''
+                if info and info.quantity and info.iconFileID then
+                    str= '|T'..info.iconFileID..':0|t'..info.quantity..'\n'
+                end
+                return str..(e.onlyChinese and '旅行者日志进度' or MONTHLY_ACTIVITIES_PROGRESSED)
+            end
+            down= function() ToggleEncounterJournal() end
+
+        elseif text=='durabiliy' then
+            label.tooltip= e.onlyChinese and '耐久度' or DURABILITY
+            down= function() ToggleCharacter("PaperDollFrame"); end
+        elseif text=='equipmentLevel' then
+            label.tooltip= e.onlyChinese and '物品等级' or STAT_AVERAGE_ITEM_LEVEL 
+            down= function() ToggleCharacter("PaperDollFrame"); end
         end
-        e.tips:AddDoubleLine(id, addName)
-        e.tips:Show()
-        button:SetButtonState('PUSHED')
-    end)
-    label:SetScript('OnLeave', function(self2)
-        button:SetButtonState('NORMAL')
-        e.tips:Hide()
-    end)
-    if down then
-        label:SetScript('OnMouseDown', down)
+
+        label:EnableMouse(true)
+        label:SetScript('OnEnter', function(self2)
+            e.tips:SetOwner(self2, "ANCHOR_LEFT")
+            e.tips:ClearLines()
+            if self2.tooltip then
+                e.tips:AddLine(type(self2.tooltip)=='function' and self2.tooltip() or self2.tooltip)
+            end
+            e.tips:Show()
+            button:SetButtonState('PUSHED')
+        end)
+        label:SetScript('OnLeave', function(self2)
+            button:SetButtonState('NORMAL')
+            e.tips:Hide()
+        end)
+        if down  then
+            label:SetScript('OnMouseDown', down)
+        end
+    else
+        label:EnableMouse(false)
+        label:SetScript('OnEnter', nil)
+        label:SetScript('OnLeave', nil)
+        label:SetScript('OnMouseDown', nil)
     end
+
     return label
 end
 
@@ -83,10 +103,8 @@ local function set_Money()
     if money>=10000 then
         if Save.parent then
             Labels.money:SetText(e.MK(money/1e4, Save.moneyBit or 0))
-        elseif e.Player.useColor then
-            Labels.money:SetText(e.MK(money/1e4, Save.moneyBit or 0)..'|TInterface/moneyframe/ui-goldicon:8|t')
         else
-            Labels.money:SetText(e.MK(money/1e4, Save.moneyBit or 0)..'|TInterface/moneyframe/ui-silvericon:8|t')
+            Labels.money:SetText(e.MK(money/1e4, Save.moneyBit or 0)..'|TInterface/moneyframe/ui-goldicon:0|t ')
         end
     else
         Labels.money:SetText(GetMoneyString(money,true))
@@ -95,9 +113,7 @@ end
 local function set_Money_Event()
     if Save.money then
         panel:RegisterEvent('PLAYER_MONEY')
-        if not Labels.money then
-            Labels.money= create_lable(button, 'money')
-        end
+        Labels.money= create_Set_lable(button, 'money')--建立,或设置,Labels
         set_Money()
     else
         panel:UnregisterEvent('PLAYER_MONEY')
@@ -120,10 +136,10 @@ local function set_Durabiliy()
             m =m + max;
         end
     end
-    local du, value= nil, 100
+    local du, value=nil, 100
     if m>0 then
         value = floor((c/m) * 100)
-        du= format('%i%%', value)--'|T132281:8|t'
+        du= format('%i%%', value)
         if value<30 then
             du='|cnRED_FONT_COLOR:'..du..'|r';
         elseif value<=60 then
@@ -132,8 +148,13 @@ local function set_Durabiliy()
             du='|cnGREEN_FONT_COLOR:'..du..'|r';
         end
     end
-    Labels.durabiliy:SetText(du or '')
-    e.Set_HelpTips({frame=button, topoint=Labels.durabiliy, point='left', size={40,40}, color={r=1,g=0,b=0,a=1}, onlyOne=true, show=value<=40})--设置，提示
+    if not Save.parent and du then
+        du= du..'|T132281:0|t '
+    end
+    if Labels.durabiliy then
+        Labels.durabiliy:SetText(du or '')
+        e.Set_HelpTips({frame=button, topoint=Labels.durabiliy, point='left', size={40,40}, color={r=1,g=0,b=0,a=1}, onlyOne=true, show=value<=40})--设置，提示
+    end
     return du or ''
 end
 local function set_EquipmentLevel()--装等
@@ -146,7 +167,7 @@ local function set_EquipmentLevel()--装等
             red= true
         end
         if not Save.parent then
-            text= text..(e.Player.sex==2 and '|A:charactercreate-gendericon-male:0:0|a' or '|A:charactercreate-gendericon-female:0:0|a')
+            text= text..(e.Player.sex==2 and '|A:charactercreate-gendericon-male-selected:0:0|a ' or '|A:charactercreate-gendericon-female-selected:0:0|a ')
         end
     end
     Labels.equipmentLevel:SetText(text or '')
@@ -162,9 +183,7 @@ local function set_Durabiliy_EquipLevel_Event()--设置装等,耐久度,事件
     end
 
     if Save.equipmetLevel then
-        if not Labels.equipmentLevel then
-            Labels.equipmentLevel= create_lable(button, 'equipmentLevel')
-        end
+        Labels.equipmentLevel= create_Set_lable(button, 'equipmentLevel')--建立,或设置,Labels
         C_Timer.After(2, set_EquipmentLevel) --角色图标显示装等  
     else
         if Labels.equipmentLevel then
@@ -174,9 +193,7 @@ local function set_Durabiliy_EquipLevel_Event()--设置装等,耐久度,事件
 
     if Save.durabiliy then
         panel:RegisterEvent("UPDATE_INVENTORY_DURABILITY")
-        if not Labels.durabiliy then
-            Labels.durabiliy= create_lable(button, 'durabiliy')
-        end
+        Labels.durabiliy= create_Set_lable(button, 'durabiliy')--建立,或设置,Labels
         set_Durabiliy()
     else
         panel:UnregisterEvent("UPDATE_INVENTORY_DURABILITY")
@@ -191,13 +208,17 @@ local function set_Fps_Ms(self, elapsed)
     self.elapsed = self.elapsed + elapsed
     if self.elapsed > 0.4 then
         self.elapsed = 0
+        local ms = select(4, GetNetStats()) or 0--ms
+        local fps=GetFramerate() or 0
+        fps=math.modf(fps)
 
-        local value = select(4, GetNetStats()) or 0--ms
-        Labels.ms:SetText((value>400 and '|cnRED_FONT_COLOR:'..value..'|r' or value>120 and ('|cnYELLOW_FONT_COLOR:'..value..'|r') or value)..'ms')
-
-        value=GetFramerate() or 0
-        value=math.modf(value)
-        Labels.fps:SetText((value<10 and '|cnGREEN_FONT_COLOR:'..value..'|r' or value<20 and '|cnYELLOW_FONT_COLOR:'..value..'|r' or value)..'fps')
+        if Save.parent then
+            Labels.ms:SetText((ms>400 and '|cnRED_FONT_COLOR:'..ms..'|r' or ms>120 and ('|cnYELLOW_FONT_COLOR:'..ms..'|r') or ms)..'ms')
+            Labels.fps:SetText((fps<10 and '|cnGREEN_FONT_COLOR:'..math.modf(fps)..'|r' or fps<20 and '|cnYELLOW_FONT_COLOR:'..math.modf(fps)..'|r' or math.modf(fps))..'fps')
+        else
+            Labels.ms:SetText((ms>400 and '|cnRED_FONT_COLOR:'..ms..'|r' or ms>120 and ('|cnYELLOW_FONT_COLOR:'..ms..'|r') or ms)..'ms')
+            Labels.fps:SetText((fps<10 and '|cnGREEN_FONT_COLOR:'..math.modf(fps)..'|r' or fps<20 and '|cnYELLOW_FONT_COLOR:'..math.modf(fps)..'|r' or math.modf(fps))..'fps ')
+        end
     end
 end
 local function set_Fps_Ms_Show_Hide()--设置, fps, ms, 数值
@@ -209,29 +230,14 @@ local function set_Fps_Ms_Show_Hide()--设置, fps, ms, 数值
             Labels.ms:SetText('')
         end
     else
-        if not Save.hideFpsMs and not Labels.fps then
-            Labels.fps= create_lable(button, 'fps')
-            Labels.ms= create_lable(button, 'ms')
+        if not Save.hideFpsMs then
+            Labels.fps= create_Set_lable(button, 'fps')--建立,或设置,Labels
+            Labels.ms= create_Set_lable(button, 'ms')--建立,或设置,Labels
             panel:HookScript("OnUpdate", set_Fps_Ms)
         end
     end
 end
 
-
-
---[[local function set_System_FPSMS()--设置系统fps ms
-    local frame=FramerateLabel
-    if Save.SystemFpsMs then
-        if not frame or not frame:IsShown() then
-            ToggleFramerate()
-        end
-    else
-        if frame and frame:IsShown() then
-            ToggleFramerate()
-        end
-    end
-end
-]]
 
 
 
@@ -243,16 +249,18 @@ end
 local function set_perksActivitiesLastPoints_CVar()--贸易站, 点数
     local lastPoints = tonumber(GetCVar("perksActivitiesLastPoints"));
     if lastPoints and lastPoints>0 then
-        Labels.perksPoints:SetFormattedText('%i%%', lastPoints/1000*100)
+        if Save.parent then
+            Labels.perksPoints:SetFormattedText('%i%%', lastPoints/1000*100)
+        else
+            Labels.perksPoints:SetText(format('%i%%', lastPoints/1000*100)..'|A:activities-complete-diamond:0:0|a')
+        end
     else
         Labels.perksPoints:SetText('')
     end
 end
 local function set_perksActivitiesLastPoints_Event()
     if Save.perksPoints then
-        if not Labels.perksPoints then
-            Labels.perksPoints= create_lable(button, 'perksPoints')
-        end
+        Labels.perksPoints= create_Set_lable(button, 'perksPoints')--建立,或设置,Labels
         panel:RegisterEvent('CVAR_UPDATE')
         set_perksActivitiesLastPoints_CVar()
     else
@@ -301,7 +309,7 @@ local function set_Label_Point(clear)--设置 Label Poinst
                     label:SetParent(MainMenuMicroButton)
 
                 elseif text=='money' then
-                    label:SetPoint('TOP', MainMenuBarBackpackButton, 0,6)
+                    label:SetPoint('TOP', MainMenuBarBackpackButton,0,-6)
                     label:SetParent(MainMenuBarBackpackButton)
 
                 elseif text=='perksPoints' then
@@ -338,6 +346,7 @@ local function InitMenu(self, level, type)--主菜单
             func= function()
                 Save.moneyWoW= not Save.moneyWoW and true or nil
                 set_Money_Event()
+                set_Label_Point(true)
             end
         }
         e.LibDD:UIDropDownMenu_AddButton(info,level)
@@ -352,19 +361,12 @@ local function InitMenu(self, level, type)--主菜单
         func= function()
             Save.hideFpsMs= not Save.hideFpsMs and true or nil
             set_Fps_Ms_Show_Hide()--设置, fps, ms, 数值
+            set_Label_Point(true)
         end
     }
     e.LibDD:UIDropDownMenu_AddButton(info,level)
 
-    --[[info={
-        text= (e.onlyChinese and '系统' or SYSTEM).. ' fps ms',
-        checked= Save.SystemFpsMs,
-        func= function()
-            Save.SystemFpsMs= not Save.SystemFpsMs and true or nil
-            set_System_FPSMS()--设置系统fps ms
-        end
-    }
-    e.LibDD:UIDropDownMenu_AddButton(info,level)]]
+
 
     local numPlayer, allMoney, text  = 0, 0, ''
     for guid, infoMoney in pairs(WoWDate) do
@@ -388,9 +390,25 @@ local function InitMenu(self, level, type)--主菜单
         func= function()
             Save.money= not Save.money and true or nil
             set_Money_Event()--设置, 钱, 事件
+            set_Label_Point(true)
         end
     }
     e.LibDD:UIDropDownMenu_AddButton(info,level)
+
+
+    info={
+        text= (e.onlyChinese and '旅行者日志进度' or MONTHLY_ACTIVITIES_PROGRESSED),
+        checked=Save.perksPoints,
+        tooltipOnButton=true,
+        tooltipTitle= (e.onlyChinese and '旅行者日志进度' or MONTHLY_ACTIVITIES_PROGRESSED)..'|cnGREEN_FONT_COLOR:'..numPlayer..'|r  '..(e.onlyChinese and '总计: ' or FROM_TOTAL)..'|cnGREEN_FONT_COLOR:'..(allMoney >=10000 and e.MK(allMoney/10000, 3) or GetCoinTextureString(allMoney, true))..'|r',
+        func= function()
+            Save.perksPoints= not Save.perksPoints and true or nil
+            set_perksActivitiesLastPoints_Event()--贸易站, 点数
+            set_Label_Point(true)
+        end
+    }
+    e.LibDD:UIDropDownMenu_AddButton(info,level)
+     
 
     info={
         text= (e.onlyChinese and '耐久度' or DURABILITY)..': '..set_Durabiliy(),
@@ -398,6 +416,7 @@ local function InitMenu(self, level, type)--主菜单
         func= function()
             Save.durabiliy = not Save.durabiliy and true or false
             set_Durabiliy_EquipLevel_Event()--设置装等,耐久度,事件
+            set_Label_Point(true)
         end
     }
     e.LibDD:UIDropDownMenu_AddButton(info,level)
@@ -408,6 +427,7 @@ local function InitMenu(self, level, type)--主菜单
         func= function()
             Save.equipmetLevel= not Save.equipmetLevel and true or nil
             set_Durabiliy_EquipLevel_Event()--设置装等,耐久度,事件
+            set_Label_Point(true)
         end
     }
     e.LibDD:UIDropDownMenu_AddButton(info,level)
@@ -416,24 +436,11 @@ local function InitMenu(self, level, type)--主菜单
     info={
         text= (e.onlyChinese and '框架' or DEBUG_FRAMESTACK)..' MicroMenu',
         checked= Save.parent,
+        colorCode= not StoreMicroButton:IsVisible() and '|cff606060',
         func= function()
             Save.parent= not Save.parent and true or nil
             set_Label_Point(true)--设置parent
         end
-    }
-    e.LibDD:UIDropDownMenu_AddButton(info,level)
-
-    info={
-        text=e.Icon.mid..(e.onlyChinese and '缩放' or UI_SCALE)..': '..(Save.size or 12),
-        isTitle=true,
-        notCheckable=true,
-    }
-    e.LibDD:UIDropDownMenu_AddButton(info,level)
-
-    info={
-        text=e.Icon.right..(e.onlyChinese and '移动' or NPE_MOVE),
-        isTitle=true,
-        notCheckable=true,
     }
     e.LibDD:UIDropDownMenu_AddButton(info,level)
 
@@ -505,22 +512,40 @@ local function Init()
         print(id, addName, e.onlyChinese and '字体大小' or FONT_SIZE,'|cnGREEN_FONT_COLOR:'..size)
     end)
 
-    button:SetScript('OnMouseDown', function(self, d)
+    button:SetScript('OnClick', function(self, d)
         if d=='RightButton' then--移动光标
             SetCursor('UI_MOVE_CURSOR')
-        else
             if not self.Menu then
                 self.Menu=CreateFrame("Frame", id..addName..'Menu', self, "UIDropDownMenuTemplate")
                 e.LibDD:UIDropDownMenu_Initialize(self.Menu, InitMenu, 'MENU')
             end
             e.LibDD:ToggleDropDownMenu(1, nil, self.Menu, self, 15, 0)
+        elseif d=='LeftButton' then
+            ToggleFramerate()--FramerateLabel
         end
     end)
+    button:SetScript('OnLeave', function() e.tips:Hide() end)
+    button:SetScript('OnEnter', function(self2)
+        e.tips:SetOwner(self2, "ANCHOR_LEFT")
+        e.tips:ClearLines()
+        e.tips:AddDoubleLine(e.onlyChinese and '每秒帧数' or FRAMERATE_FREQUENCY, format("%.1f", GetFramerate())..e.Icon.left)
+        e.tips:AddDoubleLine(e.onlyChinese and '菜单' or SLASH_TEXTTOSPEECH_MENU, e.Icon.right)
+        e.tips:AddDoubleLine(e.onlyChinese and '移动' or NPE_MOVE, e.Icon.right)
+        e.tips:AddDoubleLine(e.onlyChinese and '缩放' or UI_SCALE, (Save.size or 12)..e.Icon.mid)
+        e.tips:AddDoubleLine(id, addName)
+        e.tips:Show()
+        button:SetButtonState('PUSHED')
+    end)
+    
+
     button:SetButtonState('PUSHED')
     C_Timer.After(4, function()
         button:SetButtonState('NORMAL')
     end)
 
+    hooksecurefunc(FramerateDisplayMixin, 'UpdateAnchor', function()
+        print(id,addName)
+    end)
 
     --#########
     --添加版本号
