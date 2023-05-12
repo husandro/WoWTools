49e37add91bbadc531a25be1c31da2bed0da14c2
husandro <husandro@qq.com>
2023-05-12 14:33:01 +0200

    Update _22OpenItems.lua

--- Tools/_22OpenItems.lua
+++ Tools/_22OpenItems.lua
@@ -248,27 +248,6 @@ local function getItems()--取得背包物品信息
                                 setAtt(bag, slot, info.iconFileID, info.itemID)
                                 return
                             end
-                            --[[if classID==0 and subclassID==8  then--声望，物品
-                                setAtt(bag, slot, info.iconFileID, info.itemID)
-                                return
-                            else
-                                --print(C_Item.IsItemSpecificToPlayerClass(info.hyperlink), info.hyperlink)
-                            if C_Item.IsItemSpecificToPlayerClass(info.hyperlink) then
-                                setAtt(bag, slot, info.iconFileID, info.itemID)
-                                return
-                            else]]
-                            --[[if classID==15 and subclassID==4 then--其它
-                                if info.itemID == 193201 or info.itemID==191251 then--钥匙框架
-                                    if GetItemCount(193201)>=3 and GetItemCount(191251)>=30 then
-                                        setAtt(bag, slot, info.iconFileID, info.itemID)
-                                        return
-                                    end
-                                elseif not C_Item.IsAnimaItemByID(info.hyperlink) then
-                                    
-                                    setAtt(bag, slot, info.iconFileID, info.itemID)
-                                    return
-                                end
-                            end]]
                         end
                     end
                 end
