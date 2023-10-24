97ab7a12417fbf14c8f3ddde83bf596cd998b796
husandro <husandro@qq.com>
2023-10-24 18:10:10 +0200

    Update HunterPet.lua

--- Other/HunterPet.lua
+++ Other/HunterPet.lua
@@ -1,5 +1,5 @@
 local id, e= ...
-if e.Player.class~='HUNTER' or IsAddOnLoaded("ImprovedStableFrame") then
+if e.Player.class~='HUNTER' then --or IsAddOnLoaded("ImprovedStableFrame") then
     return
 end
 
@@ -13,7 +13,8 @@ local ISF_SearchInput
 local maxSlots = NUM_PET_STABLE_PAGES * NUM_PET_STABLE_SLOTS
 local NUM_PER_ROW=15
 
-local function set_Slot_Talent(button, petSlot)
+
+local function set_Slot_Talent(button, petSlot)--宠物，类型
     if button.talentText then
         local talent =petSlot and select(5, GetStablePetInfo(petSlot))
         talent = talent and e.WA_Utf8Sub(talent, 2, 5, true) or ''
@@ -59,7 +60,7 @@ end
 
 
 local function Create_Text(btn, index)--创建，提示内容
-    btn.solotText= e.Cstr(btn, {layer='BACKGROUND', color={r=1,g=1,b=1,a=0.3}})
+    btn.solotText= e.Cstr(btn, {layer='BACKGROUND', color={r=1,g=1,b=1,a=0.2}})
     btn.solotText:SetPoint('CENTER')
     btn.solotText:SetText(index)
 
@@ -67,10 +68,11 @@ local function Create_Text(btn, index)--创建，提示内容
     btn.talentText:SetAlpha(1)
     btn.talentText:SetPoint('BOTTOM')
 end
+
+
+
 local function Init()
-    if not PetStableFrame then
-        return
-    end
+
     for i = 1, maxSlots do
         local btn= _G["PetStableStabledPet"..i]
         if not btn then
@@ -78,7 +80,7 @@ local function Init()
         end
         Create_Text(btn, i)--创建，提示内容
 
-        local textrue=_G['PetStableStabledPet'..i..'Background']
+        local textrue= _G['PetStableStabledPet'..i..'Background']--按钮，背景
         if textrue then
             if e.Player.useColor then
                 textrue:SetVertexColor(e.Player.useColor.r, e.Player.useColor.g, e.Player.useColor.b)
@@ -89,14 +91,36 @@ local function Init()
         end
     end
 
+    local CALL_PET_SPELL_IDS = {0883, 83242, 83243, 83244, 83245}--召唤，宠物，法术
     for i= 1, NUM_PET_ACTIVE_SLOTS do
         local btn= _G['PetStableActivePet'..i]
         if btn then
-           Create_Text(btn, i)
+            Create_Text(btn, i)--创建，提示内容
+            if CALL_PET_SPELL_IDS[i] then--召唤，宠物，法术
+                local texture= btn:CreateTexture()
+                texture:SetSize(22,22)
+                texture:SetPoint('RIGHT', btn, 'LEFT')
+                texture.spellID= CALL_PET_SPELL_IDS[i]
+                local icon= select(3, GetSpellInfo(CALL_PET_SPELL_IDS[i])) or 132161
+                texture:SetTexture(icon)
+                texture:SetScript('OnLeave', function(self) e.tips:Hide() self:SetAlpha(1) end)
+                texture:SetScript('OnEnter', function(self)
+                    if self.spellID then
+                        e.tips:SetOwner(self, "ANCHOR_LEFT")
+                        e.tips:ClearLines()
+                        e.tips:SetSpellByID(self.spellID)
+                        e.tips:AddLine(' ')
+                        e.tips:AddDoubleLine(id, addName)
+                        e.tips:Show()
+                    end
+                    self:SetAlpha(0.5)
+                end)
+            end
         end
+        --local label= _G['PetStableActivePet'..i..'PetName']
     end
 
-    local layer=PetStableFrame:GetFrameLevel()+ 1
+    local layer=PetStableFrame:GetFrameLevel()+ 1--查询
     for i = 1, maxSlots do
         local frame = _G["PetStableStabledPet"..i]
         if i > 1 then
@@ -115,79 +139,62 @@ local function Init()
         _G["PetStableStabledPet"..i]:SetPoint("TOPLEFT", _G["PetStableStabledPet"..i-NUM_PER_ROW], "BOTTOMLEFT", 0, -5)
     end
 
+    --local frame = CreateFrame("Frame", nil, "ImprovedStableFrameSlots", PetStableFrame, "InsetFrameTemplate")
+    --frame:ClearAllPoints()
+    --frame:SetSize(640, 550)
 
+    --frame:SetPoint(PetStableFrame.Inset:GetPoint(1))
+    --PetStableFrame.Inset:SetPoint("TOPLEFT", frame, "TOPRIGHT")
 
-    
-
-    local frame = CreateFrame("Frame", nil, PetStableFrame)-- "ImprovedStableFrameSlots", PetStableFrame, "InsetFrameTemplate")
-    frame:ClearAllPoints()
-    frame:SetSize(640, 550)
-
-    frame:SetPoint(PetStableFrame.Inset:GetPoint(1))
-    PetStableFrame.Inset:SetPoint("TOPLEFT", frame, "TOPRIGHT")
-
-
-    --PetStableFrameModelBg
-    --PetStableFrameModelBg:SetHeight(281 + heightDelta)
-
-    --[[local p, r, rp, x, y = PetStableModelScene:GetPoint(1)
-    PetStableModelScene:SetPoint(p, r, rp, x, y - 32)]]
-
-    PetStableStabledPet1:ClearAllPoints()
-    PetStableStabledPet1:SetPoint("TOPLEFT", frame, 8, -8)
-
-
+    --查询
     ISF_SearchInput = CreateFrame("EditBox", nil, PetStableFrame, "SearchBoxTemplate")
     if ISF_SearchInput.Middle then
         ISF_SearchInput.Middle:SetAlpha(0.5)
         ISF_SearchInput.Right:SetAlpha(0.5)
         ISF_SearchInput.Left:SetAlpha(0.5)
     end
-    --ISF_SearchInput:SetPoint("TOPLEFT", 9, 0)
-    --ISF_SearchInput:SetPoint("RIGHT", -3, 0)
-    ISF_SearchInput:SetSize(360,20)
-    ISF_SearchInput:SetPoint('BOTTOMRIGHT',-8, 8)
+
+    ISF_SearchInput:SetSize(270,20)
+    ISF_SearchInput:SetPoint('BOTTOMRIGHT',-6, 10)
+    ISF_SearchInput:SetScale(1.2)
 
     ISF_SearchInput:HookScript("OnTextChanged", ImprovedStableFrame_Update)
     ISF_SearchInput.Instructions:SetText(e.onlyChinese and '名称，类型，天赋' or (NAME .. ", " .. TYPE .. ", " .. TALENT))
 
+    PetStableStabledPet1:ClearAllPoints()--设置，200个按钮，第一个位置
+    PetStableStabledPet1:SetPoint("TOPLEFT", PetStableFrame, 97, -37)
 
-
-
-
-
-
-    PetStableFrame:SetSize(720, 630)
+    PetStableFrame:SetSize(720, 630)--设置，大小
     PetStableFrameInset.NineSlice:Hide()
 
-    PetStableModelScene:ClearAllPoints()
+    PetStableModelScene:ClearAllPoints()--设置，3D，位置
     PetStableModelScene:SetPoint('RIGHT', PetStableFrame, 'LEFT')
     PetStableModelScene:SetSize( PetStableFrame:GetHeight(),  PetStableFrame:GetHeight())
 
-    if PetStableFrameModelBg:IsShown() then
+    if PetStableFrameModelBg:IsShown() then--3D，背景
         PetStableFrameModelBg:ClearAllPoints()
         PetStableFrameModelBg:SetAllPoints(PetStableModelScene)
         PetStableFrameModelBg:SetAlpha(0.3)
         PetStableFrameInset.Bg:Hide()
     end
 
-    PetStablePetInfo:ClearAllPoints()
-    --PetStablePetInfo:SetPoint('BOTTOMLEFT', PetStableModelScene,'BOTTOMLEFT')
-    PetStableSelectedPetIcon:ClearAllPoints()
-    PetStableSelectedPetIcon:SetPoint('RIGHT', PetStableFrame, 'LEFT')
+    PetStablePetInfo:ClearAllPoints()--隐藏，宠物，信息
 
-    PetStableNameText:ClearAllPoints()
+    PetStableSelectedPetIcon:ClearAllPoints()--提示，当前，宠物，图标
+    PetStableSelectedPetIcon:SetPoint('RIGHT', PetStableFrame, 'LEFT', -4,0)
+
+    PetStableNameText:ClearAllPoints()--，当前，宠物，名称
     PetStableNameText:SetPoint('BOTTOMRIGHT', PetStableSelectedPetIcon, 'TOPRIGHT')
     PetStableNameText:SetJustifyH('RIGHT')
 
-    PetStableDiet:ClearAllPoints()
+    PetStableDiet:ClearAllPoints()--食物，提示，图标
     PetStableDiet:SetPoint('TOPRIGHT', PetStableSelectedPetIcon, 'BOTTOMRIGHT')
     PetStableDiet:SetSize(PetStableSelectedPetIcon:GetSize())
 
-    PetStableTypeText:ClearAllPoints()
+    PetStableTypeText:ClearAllPoints()--宠物，类型，文本
     PetStableTypeText:SetPoint('TOPRIGHT', PetStableDiet, 'BOTTOMRIGHT')
 
-    PetStableNextPageButton:Hide()
+    PetStableNextPageButton:Hide()--隐藏
     PetStablePrevPageButton:Hide()
     PetStableBottomInset:Hide()
 
@@ -207,25 +214,33 @@ panel:RegisterEvent('PET_STABLE_SHOW')
 panel:SetScript("OnEvent", function(self, event, arg1)
     if event == "ADDON_LOADED" then
         if arg1==id then
-            Save= WoWToolsSave[addName] or Save
+            if PetStableFrame then
 
-            --添加控制面板
-            e.AddPanel_Check({
-                name= '|A:groupfinder-icon-class-hunter:0:0|a'..(e.onlyChinese and '猎人兽栏' or addName),
-                tooltip= nil,
-                value= not Save.disabled,
-                func= function()
-                    Save.disabled = not Save.disabled and true or nil
-                    print(id, addName, e.GetEnabeleDisable(not Save.disabled), e.onlyChinese and '需求重新加载' or REQUIRES_RELOAD)
+                Save= WoWToolsSave[addName] or Save
+                --添加控制面板
+                e.AddPanel_Check({
+                    name= '|A:groupfinder-icon-class-hunter:0:0|a'..(e.onlyChinese and '猎人兽栏' or addName),
+                    tooltip= nil,
+                    value= not Save.disabled,
+                    func= function()
+                        Save.disabled = not Save.disabled and true or nil
+                        print(id, addName, e.GetEnabeleDisable(not Save.disabled), e.onlyChinese and '需求重新加载' or REQUIRES_RELOAD)
+                    end
+                })
+
+                if Save.disabled  then-- or IsAddOnLoaded("ImprovedStableFrame") then
+                    panel:UnregisterAllEvents()
+                else
+                    Init()
+                    if IsAddOnLoaded("ImprovedStableFrame") then
+                        print(id, addName,
+                            e.GetEnabeleDisable(false), 'Improved Stable Frame',
+                            e.onlyChinese and '插件' or ADDONS
+                        )
+                    end
                 end
-            })
 
-            if Save.disabled or IsAddOnLoaded("ImprovedStableFrame") then
-               panel:UnregisterAllEvents()
-            else
-                Init()
             end
-
             self:UnregisterEvent('ADDON_LOADED')
             panel:RegisterEvent('PLAYER_LOGOUT')
         end
