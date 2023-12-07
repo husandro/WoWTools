fb286bf4b817e1c1b2a26ff1885c2ee663614a3a
husandro <husandro@qq.com>
2023-12-07 17:12:25 +0100

    1

---
+++ Other/Chinese.lua
@@ -0,0 +1,61 @@
+local id, e= ...
+if LOCALE_zhCN or LOCALE_zhTW or not e.Player.husandro then
+    return
+end
+
+
+local addName= BUG_CATEGORY15
+local panel= CreateFrame("Frame")
+
+
+
+
+
+
+local function Init()
+    --角色
+    
+    
+end
+
+
+
+
+
+
+
+
+--###########
+--加载保存数据
+--###########
+panel:RegisterEvent("ADDON_LOADED")
+panel:RegisterEvent('PLAYER_LOGOUT')
+panel:SetScript("OnEvent", function(self, event, arg1)
+    if event == "ADDON_LOADED" then
+        if arg1==id then
+            Save= WoWToolsSave[addName] or Save
+
+            --添加控制面板
+            e.AddPanel_Check({
+                name= addName,
+                tooltip= addName,
+                value= not Save.disabled,
+                func= function()
+                    Save.disabled = not Save.disabled and true or nil
+                    print(id, addName, e.GetEnabeleDisable(not Save.disabled), e.onlyChinese and '需求重新加载' or REQUIRES_RELOAD)
+                end
+            })
+
+            if not Save.disabled then
+              Init()
+            end
+
+            self:UnregisterEvent('ADDON_LOADED')
+        end
+
+    elseif event == "PLAYER_LOGOUT" then
+        if not e.ClearAllSave then
+            WoWToolsSave[addName]=Save
+        end
+    end
+end)
