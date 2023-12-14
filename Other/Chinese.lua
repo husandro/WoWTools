93110908f059f7ab975c259796a5910cc642e0d9
husandro <husandro@qq.com>
2023-12-14 17:10:52 +0100

    1

--- Other/Chinese.lua
+++ Other/Chinese.lua
@@ -45,10 +45,191 @@ local function Init()
         set(self.BorderBox.SelectedIconArea.SelectedIconText.SelectedIconDescription, '点击在列表中浏览')
     end)
 
-    --法术
-    set(SpellBookFrameTabButton1, '法术')
-    set(SpellBookFrameTabButton2, '专业')
-    set(SpellBookFrameTabButton3, '宠物')
+    PAPERDOLL_SIDEBARS[1].name= '角色属性'
+    PAPERDOLL_SIDEBARS[2].name= '头衔'
+    PAPERDOLL_SIDEBARS[3].name= '装备管理'
+
+    --ReputationFrame.xml
+    set(ReputationDetailViewRenownButton, '浏览名望')
+    set(ReputationDetailMainScreenCheckBoxText, '显示为经验条')
+    REPUTATION_SHOW_AS_XP = "在你的技能栏上方显示声望栏。";
+    set(ReputationDetailInactiveCheckBoxText, '隐藏')
+    REPUTATION_MOVE_TO_INACTIVE = "将声望条移动到你的声望列表最底部的隐藏分类中。对于归类你不再关心的声望非常有用。";
+    set(ReputationDetailAtWarCheckBoxText, '交战状态')
+    REPUTATION_AT_WAR_DESCRIPTION = "决定该阵营的成员对你的反应。如果你钩选了交战状态框，那么你就可以攻击他们。如果你没有钩选交战状态框，则不会对他们进行攻击。";
+
+    set(TokenFramePopup.Title, '货币设置')
+    set(TokenFramePopup.InactiveCheckBox.Text, '未使用')
+    TOKEN_MOVE_TO_UNUSED = "将这种货币移动到你的列表底部的“未使用”类别中。可以有效地归类那些你不再关心的货币类型。";
+    set(TokenFramePopup.BackpackCheckBox.Text, '在行囊上显示')
+    TOKEN_SHOW_ON_BACKPACK = "钩选此项可以在你的行囊上显示此类货币的数量。\n\n你也可以按住Shift点击某种货币，从行囊中添加或移除它。";
+    TOO_MANY_WATCHED_TOKENS = "你在同一时间内只能追踪%d种货币";
+
+    --PaperDollFrame.lua
+    SPELL_STAT1_NAME = "力量";
+    SPELL_STAT2_NAME = "敏捷";
+    SPELL_STAT3_NAME = "耐力";
+    SPELL_STAT4_NAME = "智力";
+    SPELL_STAT5_NAME = "精神";
+
+    MANA = "法力值";
+
+    CR_AVOIDANCE_TOOLTIP = "范围效果法术的伤害降低。\n\n闪避：%s [+%.2f%%]";
+    CR_BLOCK_TOOLTIP = "格挡可使一次攻击的伤害降低%0.2f%%.\n|cff888888（对抗与你实力相当的敌人时）|r";
+    CR_CRIT_MELEE_TOOLTIP = "攻击造成额外伤害的几率。\n%s点爆击（+%.2f%%爆击率）";
+    CR_CRIT_PARRY_RATING_TOOLTIP = "攻击和法术造成额外效果的几率。\n\n爆击：%s [+%.2f%%]\n\n招架几率提高%.2f%%。";
+    CR_CRIT_RANGED_TOOLTIP = "攻击造成额外伤害的几率。\n%s点爆击（+%.2f%%爆击率）";
+    CR_CRIT_SPELL_TOOLTIP = "法术造成额外伤害的几率。\n%s点爆击（+%.2f%%爆击率）";
+    CR_CRIT_TOOLTIP = "攻击和法术造成额外效果的几率。\n\n爆击：%s [+%.2f%%]";
+    CR_DODGE_BASE_STAT_TOOLTIP = "躲闪几率提高%.2f%%|n|cff888888（在效果递减之前）|r";
+    CR_DODGE_TOOLTIP = "%d点躲闪可使躲闪几率提高%.2f%%\n|cff888888（在效果递减之前）|r";
+    CR_EXPERTISE_TOOLTIP = "被躲闪或招架的几率降低%s\n%s点精准(+%.2F%% 精准)";
+    CR_HASTE_RATING_TOOLTIP = "%d点急速  (%.2f%% 急速)";
+    CR_HIT_MELEE_TOOLTIP = "使你的近战攻击命中%d级目标的几率提高%.2f%%。";
+    CR_HIT_RANGED_TOOLTIP = "使你的远程攻击命中%d级目标的几率提高%.2f%%。";
+    CR_HIT_SPELL_TOOLTIP = "使你的法术命中%d级目标的几率提高%.2f%%。";
+    CR_LIFESTEAL_TOOLTIP = "你所造成伤害和治疗的一部分将转而治疗你。\n\n吸血：%s [+%.2f%%]";
+    CR_MULTISTRIKE_TOOLTIP = "有%.2f%%几率对每个目标造成相当于普通伤害或治疗量%.0f%%的额外伤害或治疗。\n\n溅射：%s [%.2f%%]";
+    CR_PARRY_BASE_STAT_TOOLTIP = "招架几率提高%.2f%%|n|cff888888（在效果递减之前）|r";
+    CR_PARRY_TOOLTIP = "%d点招架可使招架几率提高%.2f%%\n|cff888888（在效果递减之前）|r";
+    CR_RANGED_EXPERTISE_TOOLTIP = "被躲闪的几率降低%s\n%s点精准(+%.2F%% 精准)";
+    CR_SPEED_TOOLTIP = "提升移动速度。|n|n速度：%s [+%.2f%%]";
+    CR_STURDINESS_TOOLTIP = "防止物品的耐久度降低。";
+    CR_VERSATILITY_TOOLTIP = "造成的伤害值和治疗量提高%.2f%%，受到的伤害降低%.2f%%。\n\n全能：%s [%.2f%%/%.2f%%]";
+
+    EFAULT_AGILITY_TOOLTIP = "提高你的远程武器攻击强度。|n提高所有武器的爆击几率。|n提高你的护甲值和躲避攻击的几率。";
+    DEFAULT_INTELLECT_TOOLTIP = "提高你的武器技能熟练度提升速度。";
+    DEFAULT_SPIRIT_TOOLTIP = "提高你的生命值和法力值回复速度。";
+    DEFAULT_STAMINA_TOOLTIP = "提高你的生命值上限。";
+    DEFAULT_STAT1_TOOLTIP = "提高你的攻击和技能强度";
+    DEFAULT_STAT2_TOOLTIP = "提高你的攻击和技能强度";
+    DEFAULT_STAT3_TOOLTIP = "生命值提高%s点";
+    DEFAULT_STAT4_TOOLTIP = "提高你的法术强度";
+    DEFAULT_STATARMOR_TOOLTIP = "受到的物理伤害减免%0.2f%%";
+    DEFAULT_STATDEFENSE_TOOLTIP = "%s点防御（+%s 防御）\n躲闪、格挡和招架几率提高%.2f%%\n被命中和被爆击的几率降低%.2f%%\n|cff888888（在效果递减之前）|r";
+    DEFAULT_STATSPELLBONUS_TOOLTIP = "法术攻击的伤害加成。";
+
+    DAMAGE_SCHOOL2 = "神圣";
+    DAMAGE_SCHOOL3 = "火焰";
+    DAMAGE_SCHOOL4 = "自然";
+    DAMAGE_SCHOOL5 = "冰霜";
+    DAMAGE_SCHOOL6 = "暗影";
+    DAMAGE_SCHOOL7 = "奥术";
+
+    STAT_ARMOR = "护甲";
+    STAT_ARMOR_BASE_TOOLTIP = "基础护甲值物理减伤：%0.2f%%";
+    STAT_ARMOR_BONUS_ARMOR_BLADED_ARMOR_TOOLTIP = "总护甲值物理减伤：%0.2f%%\n攻击强度提高%d";
+    STAT_ARMOR_TARGET_TOOLTIP = "（对当前目标：%0.2f%%）";
+    STAT_ARMOR_TOOLTIP = "物理伤害减免：%0.2f%%\n|cff888888（对抗与你实力相当的敌人时）|r";
+    STAT_ARMOR_TOTAL_TOOLTIP = "总护甲值物理减伤：%0.2f%%";
+    STAT_ATTACK_POWER = "攻击强度";
+    STAT_ATTACK_SPEED = "攻击速度";
+    STAT_ATTACK_SPEED_BASE_TOOLTIP = "攻击速度+%s%%";
+    STAT_AVERAGE_ITEM_LEVEL = "物品等级";
+    STAT_AVERAGE_ITEM_LEVEL_EQUIPPED = "（已装备%d）";
+    STAT_AVERAGE_ITEM_LEVEL_TOOLTIP = "你的装备的平均物品等级。";
+    STAT_AVERAGE_PVP_ITEM_LEVEL = "PvP物品等级：%d";
+    STAT_AVOIDANCE = "闪避";
+    STAT_BLOCK = "格挡";
+    STAT_BLOCK_TARGET_TOOLTIP = "（对当前目标：%0.2f%%）";
+    STAT_BLOCK_TOOLTIP = "格挡值提高%d";
+    STAT_BONUS_ARMOR = "护甲加成";
+    STAT_CATEGORY_ATTACK = "攻击";
+    STAT_CATEGORY_ATTRIBUTES = "属性";
+    STAT_CATEGORY_DEFENSE = "防御";
+    STAT_CATEGORY_ENHANCEMENTS = "强化属性";
+    STAT_CATEGORY_GENERAL = "概况";
+    STAT_CATEGORY_MELEE = "近战";
+    STAT_CATEGORY_PVP = "PvP";
+    STAT_CATEGORY_RANGED = "远程";
+    STAT_CATEGORY_RESISTANCE = "抗性";
+    STAT_CATEGORY_SPELL = "法术";
+    STAT_CHI_TOOLTIP = "能量值上限。使用技能会消耗能量值，能量值会随时间自动回复。";
+    STAT_CRITICAL_STRIKE = "爆击";
+    STAT_DODGE = "躲闪";
+    STAT_DPS_SHORT = "每秒伤害";
+    STAT_ENERGY_REGEN = "能量值回复";
+    STAT_ENERGY_REGEN_TOOLTIP = "每秒回复的能量值";
+    STAT_ENERGY_TOOLTIP = "能量值上限。使用技能会消耗能量值，能量值会随时间自动回复。";
+    STAT_EXPERTISE = "精准";
+    STAT_FOCUS_REGEN = "集中值回复";
+    STAT_FOCUS_REGEN_TOOLTIP = "每秒回复的集中值。";
+    STAT_FOCUS_TOOLTIP = "集中值上限。使用技能会消耗集中值，集中值会随时间自动回复。";
+    STAT_FORMAT = "%s：";
+    STAT_HASTE = "急速";
+    STAT_HASTE_BASE_TOOLTIP = "\n\n急速：%s [+%.2f%%]";
+    STAT_HASTE_DEATHKNIGHT_TOOLTIP = "提高攻击速度和符文回复速度。";
+    STAT_HASTE_DRUID_TOOLTIP = "提高攻击、施法和能量值回复速度。";
+    STAT_HASTE_HUNTER_TOOLTIP = "提高攻击速度和集中值回复速度。";
+    STAT_HASTE_MELEE_DEATHKNIGHT_TOOLTIP = "提高攻击速度和符文回复速度。";
+    STAT_HASTE_MELEE_DRUID_TOOLTIP = "提高攻击速度和能量值回复速度。";
+    STAT_HASTE_MELEE_HUNTER_TOOLTIP = "提高攻击速度和集中值回复速度。";
+    STAT_HASTE_MELEE_MONK_TOOLTIP = "提高攻击速度和能量值回复速度。";
+    STAT_HASTE_MELEE_ROGUE_TOOLTIP = "提高攻击速度和能量值回复速度。";
+    STAT_HASTE_MELEE_TOOLTIP = "提高攻击速度。";
+    STAT_HASTE_MONK_TOOLTIP = "提高攻击、施法和能量值回复速度。";
+    STAT_HASTE_RANGED_HUNTER_TOOLTIP = "提高攻击速度和集中值回复速度。";
+    STAT_HASTE_RANGED_TOOLTIP = "提高攻击速度。";
+    STAT_HASTE_ROGUE_TOOLTIP = "提高攻击速度和能量值回复速度。";
+    STAT_HASTE_SPELL_TOOLTIP = "提高施法速度。";
+    STAT_HASTE_TOOLTIP = "提高攻击速度和施法速度。";
+    STAT_HEALTH_PET_TOOLTIP = "最大生命值。如果生命值为0，则该生物会死亡。";
+    STAT_HEALTH_TOOLTIP = "你的生命值上限。如果你的生命值为0的话，那你就会死亡。";
+    STAT_HIT_CHANCE = "命中几率";
+    STAT_HIT_MELEE_TOOLTIP = "%s点命中（+%.2f%%命中率）";
+    STAT_HIT_NORMAL_ATTACKS = "普通攻击";
+    STAT_HIT_RANGED_TOOLTIP = "%s点命中（+%.2f%%命中率）";
+    STAT_HIT_SPECIAL_ATTACKS = "特殊攻击";
+    STAT_HIT_SPELL_TOOLTIP = "%s点命中（+%.2f%%命中率）";
+    STAT_LIFESTEAL = "吸血";
+    STAT_LUNAR_POWER_TOOLTIP = "最大星界能量值。";
+    STAT_MANA_TOOLTIP = "法力值上限。施放法术需要消耗法力值。";
+    STAT_MASTERY = "精通";
+    STAT_MASTERY_TOOLTIP = "精通： %s [+%.2f%%]";
+    STAT_MASTERY_TOOLTIP_NOT_KNOWN = "要使精通生效，你必须先在训练师处学习该技能。";
+    STAT_MASTERY_TOOLTIP_NO_TALENT_SPEC = "你必须选择一项天赋专精以激活精通技能。";
+    STAT_MOVEMENT_FLIGHT_TOOLTIP = "飞行速度：%d%%";
+    STAT_MOVEMENT_GROUND_TOOLTIP = "奔跑速度：%d%%";
+    STAT_MOVEMENT_SPEED = "移动速度";
+    STAT_MOVEMENT_SWIM_TOOLTIP = "游泳速度：%d%%";
+    STAT_MULTISTRIKE = "溅射";
+    STAT_NO_BENEFIT_TOOLTIP = "|cff808080该属性不能使你获益|r";
+    STAT_PARRY = "招架";
+    STAT_PVP_POWER = "PvP强度";
+    STAT_RAGE_TOOLTIP = "怒气值上限。使用技能会消耗怒气值，攻击敌人和受到攻击时会回复怒气值。";
+    STAT_RESILIENCE = "PvP韧性";
+    STAT_RESILIENCE_BASE_TOOLTIP = "\n%s点韧性 (+%.2f%%韧性)";
+    STAT_RUNE_REGEN = "符文速度";
+    STAT_RUNE_REGEN_FORMAT = "%.2f秒";
+    STAT_RUNE_REGEN_TOOLTIP = "每个符文充能所需的时间。";
+    STAT_RUNIC_POWER_TOOLTIP = "符文能量上限。";
+    STAT_SPEED = "加速";
+    STAT_SPELLDAMAGE = "法术伤害";
+    STAT_SPELLDAMAGE_TOOLTIP = "提高伤害性法术的效果";
+    STAT_SPELLHEALING = "法术治疗";
+    STAT_SPELLHEALING_TOOLTIP = "提高治疗性法术的效果";
+    STAT_SPELLPOWER = "法术强度";
+    STAT_SPELLPOWER_MELEE_ATTACK_POWER_TOOLTIP = "提高法术造成的伤害和治疗效果。提高近战武器造成的伤害，每秒%s点伤害。";
+    STAT_SPELLPOWER_TOOLTIP = "提高法术造成的伤害和治疗效果。";
+    STAT_STAGGER = "醉拳";
+    STAT_STAGGER_TARGET_TOOLTIP = "（对当前目标比例%0.2f%%）";
+    STAT_STAGGER_TOOLTIP = "你的醉拳可化解%0.2f%%的伤害";
+    STAT_STURDINESS = "永不磨损";
+    STAT_TARGET_LEVEL = "目标等级";
+    STAT_TEMPLATE = "%s状态";
+    STAT_TOOLTIP_BONUS_AP = "提高你的攻击和技能强度";
+    STAT_TOOLTIP_BONUS_AP_SP = "提高你的攻击和技能强度";
+    STAT_TOOLTIP_SP_AP_DRUID = "提高你的法术、攻击和技能强度";
+    STAT_USELESS_TOOLTIP = "|cff808080不能使你的职业获益|r";
+    STAT_VERSATILITY = "全能";
+
+
+    --法术 SpellBookFrame.lua
+    hooksecurefunc('SpellBookFrame_Update', function()
+        set(SpellBookFrameTabButton1, '法术')
+        set(SpellBookFrameTabButton2, '专业')
+        set(SpellBookFrameTabButton3, '宠物')
+    end)
 end
 
 
@@ -213,7 +394,7 @@ local function Init_Loaded(arg1)
         set(AuctionHouseFrameAuctionsFrameText, '一口价')
 
         set(AuctionHouseFrame.SearchBar.SearchButton, '搜索')
-        
+
         set(AuctionHouseFrame.ItemSellFrame.CreateAuctionLabel, '开始拍卖')
         set(AuctionHouseFrame.ItemSellFrame.PostButton,'创建拍卖')
         set(AuctionHouseFrame.ItemSellFrame.QuantityInput.Label, '数量')
