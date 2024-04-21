85e6ec51ce5dedc79042278e67c334a1c640849e
husandro <husandro@qq.com>
2024-04-21 17:23:59 +0200

    Update ItemInfo.lua

--- Plus/ItemInfo.lua
+++ Plus/ItemInfo.lua
@@ -406,11 +406,10 @@ local function Set_Item_Info(self, tab)
                 elseif containerInfo and itemQuality==0 then
                     topRightText= '|A:Coin-Silver:0:0|a'
                 end
-                if containerInfo and not containerInfo.isBound and (bindType==LE_ITEM_BIND_ON_EQUIP or bindType==LE_ITEM_BIND_ON_USE) and not topRightText then
-                    rightText='|A:greatVault-lock:0:0|a'--可交易
-                end
             end
-
+            if containerInfo and not containerInfo.isBound and (bindType==LE_ITEM_BIND_ON_EQUIP or bindType==LE_ITEM_BIND_ON_USE) and not topRightText then
+                rightText='|A:greatVault-lock:16:16|a'--可交易
+            end
 
         elseif battlePetSpeciesID or itemID==82800 or classID==17 or (classID==15 and subclassID==2) or itemLink:find('Hbattlepet:(%d+)') then--宠物
             local speciesID = battlePetSpeciesID or itemLink:match('Hbattlepet:(%d+)') or select(13, C_PetJournal.GetPetInfoByItemID(itemID))--宠物
