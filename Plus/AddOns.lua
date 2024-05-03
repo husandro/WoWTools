730bb3a685ba5e509d40ecd0a887e715d55b9cff
husandro <husandro@qq.com>
2024-05-03 10:55:57 +0200

    Update AddOns.lua

--- Plus/AddOns.lua
+++ Plus/AddOns.lua
@@ -791,9 +791,11 @@ local function Set_Load_Button()--LoadButtons
             local atlas = C_AddOns.GetAddOnMetadata(i, "IconAtlas")
             if texture or atlas then
                 if C_AddOns.IsAddOnLoaded(i) then
-                    table.insert(newTab, {index=i, load=true, atlas=atlas, texture=texture})
+                    table.insert(newTab, 1, {index=i, load=true, atlas=atlas, texture=texture})
                 elseif select(2, C_AddOns.IsAddOnLoadable(i))=='DEMAND_LOADED' then
-                    table.insert(newTab, {index=i, load=false, atlas=atlas, texture=texture})
+                    table.insert(newTab, 1, {index=i, load=false, atlas=atlas, texture=texture})
+                else
+                    table.insert(newTab, {index=i, disabled=true, atlas=atlas, texture=texture})
                 end
             end
         end
@@ -802,11 +804,12 @@ local function Set_Load_Button()--LoadButtons
     for i, info in pairs(newTab) do
        local btn= LoadFrame.buttons[i]
        if not btn then
-            btn= e.Cbtn(LoadFrame, {icon='hide'})--, size=22})
+            btn= e.Cbtn(LoadFrame, {icon='hide'})
             btn.texture= btn:CreateTexture()
             btn.texture:SetAllPoints(btn)
             function btn:set_alpha()
-                self:SetAlpha(self.load and 1 or 0.3)
+                self:SetAlpha(self.disabled and 0.3 or 1)
+                self.texture:SetDesaturated(self.load==false)
             end
 
             btn.Text= e.Cstr(btn)
@@ -824,9 +827,40 @@ local function Set_Load_Button()--LoadButtons
                 LoadFrame.btn:SetAlpha(0.5)
             end)
             btn:SetScript('OnEnter', function(self)
-                local findIndex= self:GetID()
                 AddonTooltip:SetOwner(self:GetParent().buttons[1], "ANCHOR_RIGHT")
                 AddonTooltip_Update(self)
+                AddonTooltip:AddLine(' ')
+                local addonIndex= self:GetID()
+                local character = UIDropDownMenu_GetSelectedValue(AddonCharacterDropDown);
+                if ( character == true ) then
+                    character = nil;
+                end                
+                local loadable, reason = C_AddOns.IsAddOnLoadable(addonIndex, character)
+                local checkboxState = C_AddOns.GetAddOnEnableState(addonIndex, character);
+                if ( not InGlue() ) then
+                    enabled = (C_AddOns.GetAddOnEnableState(addonIndex, UnitName("player")) > Enum.AddOnEnableState.None);
+                else
+                    enabled = (checkboxState > Enum.AddOnEnableState.None);
+                end
+                local col
+                if ( loadable or ( enabled and (reason == "DEP_DEMAND_LOADED" or reason == "DEMAND_LOADED") ) ) then
+                    col='|cffffc600'
+                elseif ( enabled and reason ~= "DEP_DISABLED" ) then
+                    col='|cffff1919'
+                else
+                    col='|cff999999'
+                end
+                AddonTooltip:AddDoubleLine(
+                    reason and col..(e.cn(_G["ADDON_"..reason]) or ' ') or ' ',
+                    format('%s%s', e.onlyChinese and '查询' or WHO, e.Icon.left)
+                )
+                
+                AddonTooltip:Show()
+                self:SetAlpha(1)
+                LoadFrame.btn:SetAlpha(1)
+            end)
+            btn:SetScript('OnClick', function(self)
+                local findIndex= self:GetID()
                 AddonList.ScrollBox:ScrollToElementDataIndex(findIndex)
                 for _, frame in pairs(AddonList.ScrollBox:GetFrames() or {}) do
                     if frame:GetID()==findIndex then
@@ -837,8 +871,6 @@ local function Set_Load_Button()--LoadButtons
                         break
                     end
                 end
-                self:SetAlpha(1)
-                LoadFrame.btn:SetAlpha(1)
             end)
             LoadFrame.buttons[i]= btn
        end
@@ -857,11 +889,12 @@ local function Set_Load_Button()--LoadButtons
             name= name:gsub('!', '')
             name= e.WA_Utf8Sub(name, 2, 3)
             btn.Text:SetText(name)
-            btn:SetTexture(0)
+            btn:SetNormalTexture(0)
        end
 
        btn:SetID(info.index)
        btn.load= info.load
+       btn.disabled=info.disabled
        btn:set_alpha()
        btn:SetShown(true)
     end
