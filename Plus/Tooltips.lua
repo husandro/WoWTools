339adf424ea29d648fe6e622c0cddd2bca9be0e7
husandro <husandro@qq.com>
2024-06-26 13:37:00 +0200

    Update Tooltips.lua

--- Plus/Tooltips.lua
+++ Plus/Tooltips.lua
@@ -1748,6 +1748,37 @@ local function Init()
     ShoppingTooltip2.Portrait:SetAtlas('Adventures-Target-Indicator')
     ShoppingTooltip2.Portrait:SetAlpha(0.5)
 
+    --[[TooltipDataRules.lua
+    TooltipDataType = {
+		Item = 0,
+		Spell = 1,
+		Unit = 2,
+		Corpse = 3,
+		Object = 4,
+		Currency = 5,
+		BattlePet = 6,
+		UnitAura = 7,
+		AzeriteEssence = 8,
+		CompanionPet = 9,
+		Mount = 10,
+		PetAction = 11,
+		Achievement = 12,
+		EnhancedConduit = 13,
+		EquipmentSet = 14,
+		InstanceLock = 15,
+		PvPBrawl = 16,
+		RecipeRankInfo = 17,
+		Totem = 18,
+		Toy = 19,
+		CorruptionCleanser = 20,
+		MinimapMouseover = 21,
+		Flyout = 22,
+		Quest = 23,
+		QuestPartyProgress = 24,
+		Macro = 25,
+		Debug = 26,
+	},]]
+--Blizzard_SharedXMLGame/Tooltip/TooltipDataRules.lua
     TooltipDataProcessor.AddTooltipPostCall(TooltipDataProcessor.AllTypes,  function(tooltip, data)--TooltipUtil.lua
         if tooltip==ShoppingTooltip1 or ShoppingTooltip2==tooltip then
             return
