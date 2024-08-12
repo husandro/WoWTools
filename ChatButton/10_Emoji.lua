2ce2d8ea8d13acc44842fb503cc388ed4491c9db
husandro <husandro@qq.com>
2024-08-12 14:43:57 +0200

    1

--- ChatButton/10_Emoji.lua
+++ ChatButton/10_Emoji.lua
@@ -8,13 +8,15 @@ local Save={
     --show=true
     --On_Click_Show
     clickIndex=18,
-    clickButton='RightButton',
 }
 
 local EmojiButton
 local Frame
-local File, FiileTexture
-local Texture
+
+local EmojiText
+local TextToTexture--过滤，事件
+local EmojiTextureName
+
 local Channels={
     "CHAT_MSG_CHANNEL", -- 公共频道
     "CHAT_MSG_SAY",  -- 说
@@ -35,9 +37,11 @@ local Channels={
     "CHAT_MSG_COMMUNITIES_CHANNEL", --社区聊天内容        
 }
 
+
+--过滤，事件
 local function Init_Chat_Filter(_, _, msg, ...)
     local str=msg
-    for text, icon in pairs(FiileTexture) do
+    for text, icon in pairs(TextToTexture) do
         str= str:gsub(text, icon)
     end
     if str ~=msg then
@@ -46,14 +50,13 @@ local function Init_Chat_Filter(_, _, msg, ...)
 end
 
 
-
-
 local function send(text, d)--发送信息
     text='{'..text..'}'
     if d =='LeftButton' then
-        local ChatFrameEditBox = ChatEdit_ChooseBoxForSend()
+        local ChatFrameEditBox = ChatEdit_ChooseBoxForSend() or DEFAULT_CHAT_FRAME.editBox
         ChatFrameEditBox:Insert(text)
         ChatEdit_ActivateChat(ChatFrameEditBox)
+
     elseif d=='RightButton' then
         e.Chat(text, nil, nil)
     end
@@ -76,26 +79,26 @@ local function Init_Buttons()--设置按钮
         btn:SetScript('OnClick', function(self, d)
             send(text, d)
             Save.clickIndex= self:GetID()
-            Save.clickButton=d
             EmojiButton:set_texture()
         end)
         btn:SetScript('OnEnter', function()
-            e.tips:SetOwner(Frame, "ANCHOR_RIGHT", 0,125)
+            e.tips:SetOwner(last, "ANCHOR_RIGHT")
             e.tips:ClearLines()
             e.tips:AddLine(text)
+            e.tips:AddDoubleLine(e.Icon.left..(e.onlyChinese and '插入' or 'Insert'), (e.onlyChinese and '发送' or SEND_LABEL)..e.Icon.right)
             e.tips:Show()
         end)
         btn:SetScript('OnLeave', GameTooltip_Hide)
     end
-    for i, texture in pairs(File) do
+    for i, texture in pairs(EmojiText) do
         local btn=e.Cbtn(Frame, {icon='hide',size=size, setID=i})
-        setPoint(btn, File[i])
+        setPoint(btn, EmojiText[i])
         btn:SetNormalTexture('Interface\\Addons\\WoWTools\\Sesource\\Emojis\\'..texture)
         last=btn
         index=index+1
     end
 
-    local numFile= #File
+    local numFile= #EmojiText
     for i= 1, 8 do
         local btn=e.Cbtn(Frame, {icon='hide',size=size, setID= i+numFile})
         setPoint(btn, 'rt'..i)
@@ -193,124 +196,6 @@ end
 
 
 
---#####
---主菜单
---[[#####
-local function InitMenu(_, level, type)
-    local info
-    if type then
-        for _, channel in pairs(Channels) do
-            info={
-                text=_G[channel] or channel,
-                checked=not Save.Channels[channel],
-                tooltipOnButton=true,
-                tooltipTitle= e.onlyChinese and '需要重新加载' or REQUIRES_RELOAD,
-                tooltipText=channel,
-                keepShownOnClick=true,
-                func=function()
-                    Save.Channels[channel]= not Save.Channels[channel] and true or nil
-                    print(id, e.cn(addName), e.onlyChinese and '聊天频道' or CHAT_CHANNELS,  e.onlyChinese and '需要重新加载' or REQUIRES_RELOAD)
-                end
-            }
-            e.LibDD:UIDropDownMenu_AddButton(info, level)
-        end
-        e.LibDD:UIDropDownMenu_AddSeparator(level)
-        info={
-            text= e.onlyChinese and '全选' or ALL,--全选
-            notCheckable=true,
-            keepShownOnClick=true,
-            func=function()
-                Save.Channels={}
-                print(id, e.cn(addName), e.onlyChinese and '聊天频道' or CHAT_CHANNELS,  e.onlyChinese and '需要重新加载' or REQUIRES_RELOAD)
-            end
-        }
-        e.LibDD:UIDropDownMenu_AddButton(info, level)
-        info={
-            text= e.onlyChinese and '清除' or SLASH_STOPWATCH_PARAM_STOP2,--全清
-            icon= 'bags-button-autosort-up',
-            notCheckable=true,
-            keepShownOnClick=true,
-            func=function()
-                for _, channel in pairs(Channels) do
-                    Save.Channels[channel]=true
-                end
-                print(id, e.cn(addName), e.onlyChinese and '聊天频道' or CHAT_CHANNELS,  e.onlyChinese and '需要重新加载' or REQUIRES_RELOAD)
-            end
-        }
-        e.LibDD:UIDropDownMenu_AddButton(info, level)
-
-    else
-    
-
-
-
-
-    
-        info={
-            text= e.onlyChinese and '进入战斗' or ENTERING_COMBAT,--进入战斗时, 隐藏
-            icon= 'Warfronts-BaseMapIcons-Horde-Barracks-Minimap',
-            checked=not Save.notHideCombat,
-            keepShownOnClick=true,
-            func=function()
-                Save.notHideCombat = not Save.notHideCombat and true or nil Frame:set_event()
-            end,
-            tooltipOnButton=true,
-            tooltipTitle= e.onlyChinese and '隐藏' or HIDE,
-        }
-        e.LibDD:UIDropDownMenu_AddButton(info, level)
-
-        info={
-            text= e.onlyChinese and '移动' or NPE_MOVE,--移动时, 隐藏
-            icon= 'transmog-nav-slot-feet',
-            checked=not Save.notHideMoving,
-            func=function() Save.notHideMoving = not Save.notHideMoving and true or nil Frame:set_event() end,
-            tooltipOnButton=true,
-            keepShownOnClick=true,
-            tooltipTitle= e.onlyChinese and '隐藏' or HIDE,
-        }
-        e.LibDD:UIDropDownMenu_AddButton(info, level)
-
-        info={
-            text= e.onlyChinese and '过移图标时' or ENTER_LFG..EMBLEM_SYMBOL,--过移图标时,显示
-            icon= 'newplayertutorial-drag-cursor',
-            checked=Save.showEnter,
-            keepShownOnClick=true,
-            func=function()
-                Save.showEnter = not Save.showEnter and true or nil
-            end,
-            tooltipOnButton=true,
-            tooltipTitle= e.onlyChinese and '显示' or SHOW,
-        }
-        e.LibDD:UIDropDownMenu_AddButton(info, level)
-
-        e.LibDD:UIDropDownMenu_AddSeparator(level)
-        info={
-            text= e.onlyChinese and '聊天频道' or CHAT_CHANNELS,
-            notCheckable=true,
-            menuList='Channels',
-            hasArrow=true,
-            keepShownOnClick=true,
-        }
-        e.LibDD:UIDropDownMenu_AddButton(info, level)
-        info={
-            text= e.onlyChinese and '重置' or RESET,
-            icon= 'bags-button-autosort-up',
-            notCheckable=true,
-            keepShownOnClick=true,
-            func=function()
-                Save=nil
-                e.Reload()
-            end,
-            tooltipOnButton=true,
-            tooltipTitle= e.onlyChinese and '重新加载UI' or RELOADUI,
-            colorCode='|cffff0000'
-        }
-        e.LibDD:UIDropDownMenu_AddButton(info, level)
-    end
-end]]
-
-
-
 
 
 
@@ -368,12 +253,12 @@ local function Init_Menu(self, root)
     end)
 
     sub:CreateDivider()
-    sub2=sub:CreateButton(e.onlyChinese and '聊天频道' or CHAT_CHANNELS, function()
+    sub2=sub:CreateButton((e.onlyChinese and '聊天频道' or CHAT_CHANNELS)..' '..EmojiButton.numFilter, function()
         return MenuResponse.Refresh
     end)
 
-    for _, channel in pairs(Channels) do
-        sub2:CreateCheckbox(e.cn(_G[channel]) or channel, function(data)
+    for index, channel in pairs(Channels) do
+        sub2:CreateCheckbox('|cff9e9e9e'..index..'|r '..e.cn(_G[channel]) or channel, function(data)
             return not Save.Channels[data]
         end, function(data)
             Save.Channels[data]= not Save.Channels[data] and true or nil
@@ -423,21 +308,22 @@ end
 --初始
 --1US (includes Brazil and Oceania) 2Korea 3Europe (includes Russia) 4Taiwan 5China
 local function Init()
-    Texture={'Angel','Angry','Biglaugh','Clap','Cool','Cry','Cutie','Despise','Dreamsmile','Embarrass','Evil','Excited','Faint','Fight','Flu','Freeze','Frown','Greet','Grimace','Growl','Happy','Heart','Horror','Ill','Innocent','Kongfu','Love','Mail','Makeup','Meditate','Miserable','Okay','Pretty','Puke','Shake','Shout','Shuuuu','Shy','Sleep','Smile','Suprise','Surrender','Sweat','Tear','Tears','Think','Titter','Ugly','Victory','Hero','Wronged','Mario',}
+    EmojiTextureName={'Angel','Angry','Biglaugh','Clap','Cool','Cry','Cutie','Despise','Dreamsmile','Embarrass','Evil','Excited','Faint','Fight','Flu','Freeze','Frown','Greet','Grimace','Growl','Happy','Heart','Horror','Ill','Innocent','Kongfu','Love','Mail','Makeup','Meditate','Miserable','Okay','Pretty','Puke','Shake','Shout','Shuuuu','Shy','Sleep','Smile','Suprise','Surrender','Sweat','Tear','Tears','Think','Titter','Ugly','Victory','Hero','Wronged','Mario',}
     if e.Player.region==5 then
-        File= {'天使','生气','大笑','鼓掌','酷','哭','可爱','鄙视','美梦','尴尬','邪恶','兴奋','晕','打架','流感','呆','皱眉','致敬','鬼脸','龇牙','开心','心','恐惧','生病','无辜','功夫','花痴','邮件','化妆','沉思','可怜','好','漂亮','吐','握手','喊','闭嘴','害羞','睡觉','微笑','吃惊','失败','流汗','流泪','悲剧','想','偷笑','猥琐','胜利','雷锋','委屈','马里奥'}
+        EmojiText= {'天使','生气','大笑','鼓掌','酷','哭','可爱','鄙视','美梦','尴尬','邪恶','兴奋','晕','打架','流感','呆','皱眉','致敬','鬼脸','龇牙','开心','心','恐惧','生病','无辜','功夫','花痴','邮件','化妆','沉思','可怜','好','漂亮','吐','握手','喊','闭嘴','害羞','睡觉','微笑','吃惊','失败','流汗','流泪','悲剧','想','偷笑','猥琐','胜利','雷锋','委屈','马里奥'}
     elseif e.Player.region==4 then
-        File= {'天使','生氣','大笑','鼓掌','酷','哭','可愛','鄙視','美夢','尷尬','邪惡','興奮','暈','打架','流感','呆','皺眉','致敬','鬼臉','齜牙','開心','心','恐懼','生病','無辜','功夫','花痴','郵件','化妝','沉思','可憐','好','漂亮','吐','握手','喊','閉嘴','害羞','睡覺','微笑','吃驚','失敗','流汗','流淚','悲劇','想','偷笑','猥瑣','勝利','英雄','委屈','馬里奧'}
+        EmojiText= {'天使','生氣','大笑','鼓掌','酷','哭','可愛','鄙視','美夢','尷尬','邪惡','興奮','暈','打架','流感','呆','皺眉','致敬','鬼臉','齜牙','開心','心','恐懼','生病','無辜','功夫','花痴','郵件','化妝','沉思','可憐','好','漂亮','吐','握手','喊','閉嘴','害羞','睡覺','微笑','吃驚','失敗','流汗','流淚','悲劇','想','偷笑','猥瑣','勝利','英雄','委屈','馬里奧'}
     elseif e.Player.region==2 then
-        File= {'천사','화난','웃음','박수','시원함','울음','귀엽다','경멸','꿈','당혹 스러움','악','흥분','헤일로','싸움','독감','머무르기','찡그림','공물','grimface','눈에띄는이빨','행복','심장','두려움','나쁜','순진한','쿵푸','관용구','메일','메이크업','명상','나쁨','좋은','아름다운','침','악수','외침','닥치기','수줍음','수면','웃음','놀라움','실패','땀','눈물','비극','생각하기','shirking','걱정','victory','hero','wronged','Mario'}
+        EmojiText= {'천사','화난','웃음','박수','시원함','울음','귀엽다','경멸','꿈','당혹 스러움','악','흥분','헤일로','싸움','독감','머무르기','찡그림','공물','grimface','눈에띄는이빨','행복','심장','두려움','나쁜','순진한','쿵푸','관용구','메일','메이크업','명상','나쁨','좋은','아름다운','침','악수','외침','닥치기','수줍음','수면','웃음','놀라움','실패','땀','눈물','비극','생각하기','shirking','걱정','victory','hero','wronged','Mario'}
     else
-        File=Texture
+        EmojiText=EmojiTextureName
     end
-    EmojiButton.numFile= #File
+
+    EmojiButton.numFile= #EmojiText
     
-    FiileTexture={}
-    for index, text in pairs(File) do
-        FiileTexture['{'..text..'}']= '|TInterface\\Addons\\WoWTools\\Sesource\\Emojis\\'..Texture[index]..':0|t'
+    TextToTexture={}
+    for index, text in pairs(EmojiText) do
+        TextToTexture['{'..text..'}']= '|TInterface\\Addons\\WoWTools\\Sesource\\Emojis\\'..EmojiTextureName[index]..':0|t'
     end
 
     Init_EmojiFrame()
@@ -445,7 +331,7 @@ local function Init()
     function EmojiButton:get_emoji_text(index)
         index= index or Save.clickIndex or 18
         if index<=self.numFile then
-            return File[index]
+            return EmojiText[index]
         else
             return 'rt'..(index-self.numFile)
         end
@@ -453,7 +339,7 @@ local function Init()
     function EmojiButton:get_texture(index)
         index= index or Save.clickIndex or 18
         if index<=self.numFile then
-            return 'Interface\\Addons\\WoWTools\\Sesource\\Emojis\\'..Texture[index]
+            return 'Interface\\Addons\\WoWTools\\Sesource\\Emojis\\'..EmojiTextureName[index]
         else
             return 'Interface\\TargetingFrame\\UI-RaidTargetingIcon_'..(index-self.numFile)
         end
@@ -487,7 +373,7 @@ local function Init()
 
     EmojiButton:SetScript('OnClick', function(self, d)
         if d=='LeftButton' then
-            send(self:get_emoji_text(), Save.clickButton or d)
+            send(self:get_emoji_text(), ChatEdit_GetActiveWindow() and d or 'RightButton')
 
             if Save.On_Click_Show then
                 self:set_frame_shown(true)
@@ -500,15 +386,18 @@ local function Init()
     end)
 
 
---过滤，事件
     function EmojiButton:set_filter_event()
+        local find=0
         for _, channel in pairs(Channels) do
             if not Save.Channels[channel] then
                 ChatFrame_AddMessageEventFilter(channel, Init_Chat_Filter)
+                find= find+1
             else
                 ChatFrame_RemoveMessageEventFilter(channel, Init_Chat_Filter)
             end
         end
+        self.texture:SetDesaturated(find==0)
+        self.numFilter= find
     end
 
 --Frame, 设置, State
@@ -590,15 +479,15 @@ panel:SetScript("OnEvent", function(self, event, arg1)
 end)
 --[[
 elseif LOCALE_frFR then
-    File= {'ange','en colère','riant','applaudissements','cool','pleurant','mignon','dédain','rêve','embarras','mal','excité','halo','se battre','grippe','rester','fronçant les sourcils ','salut','grimace','voile','heureux','cœur','peur','malade','innocent','KungFu','nympho','mail','maquillage','contemplation','pauvre','bon','joli','cracher','poignée de main','crier','se taire','timide','dormir','sourire','surpris','échec','sueur','larmes','tragédie','penser','ricaner','obscène','victoire',' héros','grief','Mario'}
+    EmojiText= {'ange','en colère','riant','applaudissements','cool','pleurant','mignon','dédain','rêve','embarras','mal','excité','halo','se battre','grippe','rester','fronçant les sourcils ','salut','grimace','voile','heureux','cœur','peur','malade','innocent','KungFu','nympho','mail','maquillage','contemplation','pauvre','bon','joli','cracher','poignée de main','crier','se taire','timide','dormir','sourire','surpris','échec','sueur','larmes','tragédie','penser','ricaner','obscène','victoire',' héros','grief','Mario'}
 elseif LOCALE_deDE then
-    File= {'engel','wütend','lachen','applaus','cool','weinen','süß','verachtung','traum','verlegenheit','böse','aufgeregt','heiligenschein','kämpfen','grippe','bleiben','stirnrunzeln','gruß','grimace','segeln','glücklich','herz','angst','krank','unschuldig','KungFu','nymphoman','mail','schminke','nachdenklichkeit','arm','gut','hübsch','spucken','händedruck','schrei','halt die klappe','schüchtern','schlaf','lächeln','überrascht','versagen','schweiß','tränen','tragödie','denken','kichern','obszön','sieg','held','beschwerde','Mario'}
+    EmojiText= {'engel','wütend','lachen','applaus','cool','weinen','süß','verachtung','traum','verlegenheit','böse','aufgeregt','heiligenschein','kämpfen','grippe','bleiben','stirnrunzeln','gruß','grimace','segeln','glücklich','herz','angst','krank','unschuldig','KungFu','nymphoman','mail','schminke','nachdenklichkeit','arm','gut','hübsch','spucken','händedruck','schrei','halt die klappe','schüchtern','schlaf','lächeln','überrascht','versagen','schweiß','tränen','tragödie','denken','kichern','obszön','sieg','held','beschwerde','Mario'}
 elseif LOCALE_esES or LOCALE_esMX then
-    File= {'ángel','enojado','riendo','aplausos','guay','llorando','lindo','desdén','soñar','vergüenza','maldad','emocionado','halo','pelea','gripe','quedarse','frunciendo el ceño ','saludo','mueca','navegar','feliz','corazón','miedo','enfermo','inocente','KungFu','ninfómana','gorreo','maquillaje','contemplación','pobre','bueno','bonita','escupir','apretón de manos','gritar','cállate','tímido','dormir','sonreír','sorprendido','fallar','sudar','lágrimas','tragedia','pensar','risitas','obsceno','victoria','héroe','queja','Mario'}
+    EmojiText= {'ángel','enojado','riendo','aplausos','guay','llorando','lindo','desdén','soñar','vergüenza','maldad','emocionado','halo','pelea','gripe','quedarse','frunciendo el ceño ','saludo','mueca','navegar','feliz','corazón','miedo','enfermo','inocente','KungFu','ninfómana','gorreo','maquillaje','contemplación','pobre','bueno','bonita','escupir','apretón de manos','gritar','cállate','tímido','dormir','sonreír','sorprendido','fallar','sudar','lágrimas','tragedia','pensar','risitas','obsceno','victoria','héroe','queja','Mario'}
 elseif LOCALE_ruRU then
-    File= {'ангел','злость','смех','аплодисменты','клевые','плакать','милый','презирающий','красивая мечта','смущение','зло','возбуждение','головокружение','драка','грипп','тупость','морщины','почтение','грим','гримаса','гримаса','гримаса','гримаса','жалость','красивая','плюнь','рукопожатие','крик','заткнись','застенчивость','спать','улыбка','удивление','неудача','потение','слезы','трагедия','хохот','воровство','мелочь','победа','гром','обида','Марио'}
+    EmojiText= {'ангел','злость','смех','аплодисменты','клевые','плакать','милый','презирающий','красивая мечта','смущение','зло','возбуждение','головокружение','драка','грипп','тупость','морщины','почтение','грим','гримаса','гримаса','гримаса','гримаса','жалость','красивая','плюнь','рукопожатие','крик','заткнись','застенчивость','спать','улыбка','удивление','неудача','потение','слезы','трагедия','хохот','воровство','мелочь','победа','гром','обида','Марио'}
 elseif LOCALE_ptBR then
-    --File= {'anjo','irritado','rindo','aplauso','legal','chorando','fofo','desdém','sonho','embaraço','mal','excitado','halo','luta','gripe','fique','franzindo a testa','saudação','careta','navega','feliz','coração','medo','doente','inocente','KungFu','ninfo','correio','maquiagem','contemplação','pobre','bom','bonito','cuspir','aperto de mão','gritar','cala a boca','tímido','dormir','sorriso','surpreso','falhar','suar','lágrimas','tragédia','pensar','risada','obsceno','vitória','herói','queixa','mario'}
+    --EmojiText= {'anjo','irritado','rindo','aplauso','legal','chorando','fofo','desdém','sonho','embaraço','mal','excitado','halo','luta','gripe','fique','franzindo a testa','saudação','careta','navega','feliz','coração','medo','doente','inocente','KungFu','ninfo','correio','maquiagem','contemplação','pobre','bom','bonito','cuspir','aperto de mão','gritar','cala a boca','tímido','dormir','sorriso','surpreso','falhar','suar','lágrimas','tragédia','pensar','risada','obsceno','vitória','herói','queixa','mario'}
 elseif LOCALE_itIT then
-    File= {'angelo','arrabbiato','risata','applauso','freddo','piange','carino','disprezza','sogno','imbarazzato','cattivo','eccitato','alone','lotta','influenza','resta','accigliato ','omaggio','faccia torva','denti che colpiscono','felice','cuore','paura','ill','innocente','KungFu','idioma','mail','trucco','meditazione','povero','buono','bello','sputa','stretta di mano','grida','zitto','timido','dormiente','sorridente','sorpreso','fallimento','sudore','lacrima','tragedia','pensando','sorprendendosi','preoccupante','vittoria','hero','wronged','Mario'}
+    EmojiText= {'angelo','arrabbiato','risata','applauso','freddo','piange','carino','disprezza','sogno','imbarazzato','cattivo','eccitato','alone','lotta','influenza','resta','accigliato ','omaggio','faccia torva','denti che colpiscono','felice','cuore','paura','ill','innocente','KungFu','idioma','mail','trucco','meditazione','povero','buono','bello','sputa','stretta di mano','grida','zitto','timido','dormiente','sorridente','sorpreso','fallimento','sudore','lacrima','tragedia','pensando','sorprendendosi','preoccupante','vittoria','hero','wronged','Mario'}
 ]]
