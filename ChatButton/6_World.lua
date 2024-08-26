67849a84a133f3c3fdf2c0dde778e5580bf5b605
husandro <husandro@qq.com>
2024-08-26 10:10:43 +0200

    Update 6_World.lua

--- ChatButton/6_World.lua
+++ ChatButton/6_World.lua
@@ -1100,6 +1100,8 @@ panel:SetScript("OnEvent", function(self, event, arg1)
     if event == "ADDON_LOADED" then
         if arg1==id then
             Save= WoWToolsSave['ChatButtonWorldChannel'] or Save
+            Save.myChatFilterPlayers= Save.myChatFilterPlayers or {}
+            
             addName= '|A:tokens-WoW-generic-regular:0:0|a'..(e.onlyChinese and '频道' or CHANNEL)
             WorldButton= WoWTools_ChatButtonMixin:CreateButton('World', addName)
 
