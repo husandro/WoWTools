b58146a71826b8de4e1666ebfeeb17e7c5fa13f4
husandro <husandro@qq.com>
2024-08-26 10:14:47 +0200

    Update 6_World.lua

--- ChatButton/6_World.lua
+++ ChatButton/6_World.lua
@@ -1114,6 +1114,7 @@ panel:SetScript("OnEvent", function(self, event, arg1)
             self:UnregisterEvent('ADDON_LOADED')
 
         end
+        
     elseif event == "PLAYER_LOGOUT" then
         if not e.ClearAllSave then
             WoWToolsSave['ChatButtonWorldChannel']=Save
