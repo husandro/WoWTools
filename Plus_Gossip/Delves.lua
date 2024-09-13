1c0a3294739962128052141f79fb4825b6601e84
husandro <husandro@qq.com>
2024-09-13 21:48:18 +0200

    Update Delves.lua

--- Plus_Gossip/Delves.lua
+++ Plus_Gossip/Delves.lua
@@ -1,109 +1,116 @@
 --地下堡
 local e= select(2, ...)
-local addName
 local function Save()
     return WoWTools_GossipMixin.Save
 end
 
 
 
-local function Init()
-    DelvesDifficultyPickerFrame:HookScript('OnShow', function(self)
-        if not Save().gossip or IsModifierKeyDown() or not self.gossipOptions then
-            return
-        end
-        local num= #self.gossipOptions
-        if num==0 then
-            return
-        end
-        local Option
-        do
-            if Save().delvesDifficultyMaxLevel then
-                for i=num, 1, -1 do
-                    local option= self.gossipOptions[i] or {}
-                    if IsInGroup() then
-                        C_DelvesUI.RequestPartyEligibilityForDelveTiers(self.gossipOptions[num].gossipOptionID)
+
+
+local function Init_OnShow(self)
+    if not Save().gossip or IsModifierKeyDown() or not self.gossipOptions then
+        return
+    end
+    local num= #self.gossipOptions
+    if num==0 then
+        return
+    end
+    local Option
+    do
+        if Save().delvesDifficultyMaxLevel then
+            for i=num, 1, -1 do
+                local option= self.gossipOptions[i] or {}
+                if IsInGroup() then
+                    C_DelvesUI.RequestPartyEligibilityForDelveTiers(self.gossipOptions[num].gossipOptionID)
+                end
+
+                if option.status == Enum.GossipOptionStatus.Available or option.status == Enum.GossipOptionStatus.AlreadyComplete then
+                    DelvesDifficultyPickerFrame.DelveRewardsContainerFrame:Hide();
+                    DelvesDifficultyPickerFrame:SetSelectedLevel(option.orderIndex);
+                    DelvesDifficultyPickerFrame:UpdateWidgets(option.gossipOptionID);
+                    DelvesDifficultyPickerFrame:SetSelectedOption(option);
+                    DelvesDifficultyPickerFrame.DelveRewardsContainerFrame:SetRewards();
+                    DelvesDifficultyPickerFrame:UpdatePortalButtonState();
+
+                    if not UnitAffectingCombat('player') then
+                        SetCVar('lastSelectedDelvesTier', option.orderIndex + 1)
                     end
 
-                    if option.status == Enum.GossipOptionStatus.Available or option.status == Enum.GossipOptionStatus.AlreadyComplete then
-                        DelvesDifficultyPickerFrame.DelveRewardsContainerFrame:Hide();
-                        DelvesDifficultyPickerFrame:SetSelectedLevel(option.orderIndex);
-                        DelvesDifficultyPickerFrame:UpdateWidgets(option.gossipOptionID);
-                        DelvesDifficultyPickerFrame:SetSelectedOption(option);
-                        DelvesDifficultyPickerFrame.DelveRewardsContainerFrame:SetRewards();
-                        DelvesDifficultyPickerFrame:UpdatePortalButtonState();
-
-                        if not UnitAffectingCombat('player') then
-                            SetCVar('lastSelectedDelvesTier', option.orderIndex + 1)
-                        end
-
-                        Option=option
-                        break
-                    end
+                    Option=option
+                    break
                 end
             end
         end
+    end
 
 
-        local btn= self.EnterDelveButton
-        if btn and btn:IsEnabled() then
-            local name,itemLink
-            local option= Option or self:GetSelectedOption()
-            if option and option.name then
-                local spellLink
-                if option.spellID then
-                    local link= C_Spell.GetSpellLink(option.spellID)
-                    spellLink= e.cn(link, {spellID=option.spellID, spellLink=link, isName=true})
-                end
-                name=spellLink or e.cn(option.name)
+    local btn= self.EnterDelveButton
+    if btn and btn:IsEnabled() then
+        local name,itemLink
+        local option= Option or self:GetSelectedOption()
+        if option and option.name then
+            local spellLink
+            if option.spellID then
+                local link= C_Spell.GetSpellLink(option.spellID)
+                spellLink= e.cn(link, {spellID=option.spellID, spellLink=link, isName=true})
+            end
+            name=spellLink or e.cn(option.name)
 
 
-                for _, reward in ipairs(option.rewards or {}) do
-                    if reward.rewardType == Enum.GossipOptionRewardType.Item and reward.id then
-                        e.LoadData({type='item', id=reward.id})
-                        local item= C_Item.GetItemNameByID(reward.id)
-                        local link= WoWTools_ItemMixin:GetLink(reward.id)
-                        itemLink= (itemLink or '    ')
-                            ..(
-                                e.cn(link or item, {itemID=reward.id, itemLink=link, isName=true})
-                                or ('|T'..(C_Item.GetItemIconByID(reward.id) or 0)..':0|t')
-                            )
-                            ..'x'..(reward.quantity or 1)..' '
-                    end
-                end
-                print(e.addName, addName, '|T'..(option.icon or 0)..':0|t', name)
-                if itemLink then
-                    print(itemLink)
+            for _, reward in ipairs(option.rewards or {}) do
+                if reward.rewardType == Enum.GossipOptionRewardType.Item and reward.id then
+                    e.LoadData({type='item', id=reward.id})
+                    local item= C_Item.GetItemNameByID(reward.id)
+                    local link= WoWTools_ItemMixin:GetLink(reward.id)
+                    itemLink= (itemLink or '    ')
+                        ..(
+                            e.cn(link or item, {itemID=reward.id, itemLink=link, isName=true})
+                            or ('|T'..(C_Item.GetItemIconByID(reward.id) or 0)..':0|t')
+                        )
+                        ..'x'..(reward.quantity or 1)..' '
                 end
             end
-            btn:Click()
-            print('    |cff9e9e9e|A:NPE_Icon:0:0|aAlt', e.onlyChinese and '取消' or CANCEL)
+            print(e.addName, WoWTools_GossipMixin.addName, '|T'..(option.icon or 0)..':0|t', name)
+            if itemLink then
+                print(itemLink)
+            end
         end
-
-    end)
-
-
-
-
-    Menu.ModifyMenu("MENU_DELVES_DIFFICULTY", function(_, root)
-		local options = DelvesDifficultyPickerFrame:GetOptions();
-		if not options then
-			return;
-		end
-        root:CreateDivider()
-        local sub= root:CreateCheckbox(e.onlyChinese and '最高等级' or BEST, function()
-            return Save().delvesDifficultyMaxLevel
-        end, function()
-            Save().delvesDifficultyMaxLevel= not Save().delvesDifficultyMaxLevel and true or nil
-        end)
-        sub:SetTooltip(function(tooltip)
-            tooltip:AddLine(e.addName)
-            tooltip:AddDoubleLine(addName, e.GetEnabeleDisable(Save().delvesDifficultyMaxLevel))
-        end)
-        sub:SetEnabled(Save().gossip and true or false)
-    end)
-
+        btn:Click()
+        print('    |cff9e9e9e|A:NPE_Icon:0:0|aAlt', e.onlyChinese and '取消' or CANCEL)
+    end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+local function Init_Menu(_, root)
+    local options = DelvesDifficultyPickerFrame:GetOptions();
+    if not options then
+        return;
+    end
+    root:CreateDivider()
+    local sub= root:CreateCheckbox(e.onlyChinese and '最高等级' or BEST, function()
+        return Save().delvesDifficultyMaxLevel
+    end, function()
+        Save().delvesDifficultyMaxLevel= not Save().delvesDifficultyMaxLevel and true or nil
+    end)
+    sub:SetTooltip(function(tooltip)
+        tooltip:AddLine(e.addName)
+        tooltip:AddDoubleLine(WoWTools_GossipMixin.addName, e.GetEnabeleDisable(Save().delvesDifficultyMaxLevel))
+    end)
+    sub:SetEnabled(Save().gossip and true or false)
+end
+
+
 
 
 
@@ -116,6 +123,7 @@ end
 
 
 function WoWTools_GossipMixin:Init_Delves()
-    Init()
-    addName= self.addName
+    DelvesDifficultyPickerFrame:HookScript('OnShow', Init_OnShow)
+    Menu.ModifyMenu("MENU_DELVES_DIFFICULTY", Init_Menu)
+    
 end
