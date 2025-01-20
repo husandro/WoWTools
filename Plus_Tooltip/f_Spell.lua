3e96c6e89da4127ce24112cd94658be0d3b3b291
husandro <husandro@qq.com>
2025-01-20 18:10:30 +0100

    Update f_Spell.lua

--- Plus_Tooltip/f_Spell.lua
+++ Plus_Tooltip/f_Spell.lua
@@ -1,5 +1,55 @@
 local e= select(2, ...)
 
+--猎人兽栏，宠物
+local CALL_PET_SPELL_IDS = {
+	[0883]=1,
+	[83242]=2,
+	[83243]=3,
+	[83244]=4,
+	[83245]=5,
+}
+local function Set_HunterPet(tooltip, spellID)
+    local index= CALL_PET_SPELL_IDS[spellID]
+    local info= index and C_StableInfo.GetStablePetInfo(index)
+    if not info then
+        return
+    end
+
+    tooltip:AddDoubleLine(e.cn(info.familyName), e.cn(info.specialization))
+    --..(indexType=='petAbilities' and '|cffffff00'..(e.onlyChinese and '基础技能' or format(CLUB_FINDER_LOOKING_FOR_CLASS_SPEC, BASE_SETTINGS_TAB, ABILITIES))
+    -- (indexType=='specAbilities' and '|cnRED_FONT_COLOR:'..(e.onlyChinese and '专精技能' or format(CLUB_FINDER_LOOKING_FOR_CLASS_SPEC, SPECIALIZATION, ABILITIES)))
+
+    local icon, icon2='', ''
+    local texture
+    for _, abilitie in pairs(info.abilities or info.petAbilities or {}) do
+        texture= C_Spell.GetSpellTexture(abilitie)
+        if texture and texture>0 then
+            icon= icon..'|T'..texture..':0|t'
+        end
+    end
+    for _, abilitie in pairs(info.specAbilities or {}) do
+        texture= C_Spell.GetSpellTexture(abilitie)
+        if texture and texture>0 then
+            icon2= icon2..'|T'..texture..':0|t'
+        end
+    end
+
+    if icon~='' then
+        tooltip:AddDoubleLine(
+            e.onlyChinese and '基础技能' or format(CLUB_FINDER_LOOKING_FOR_CLASS_SPEC, BASE_SETTINGS_TAB, ABILITIES),
+            icon
+        )
+    end
+    if icon2~='' then
+        tooltip:AddDoubleLine(
+            e.onlyChinese and '专精技能' or format(CLUB_FINDER_LOOKING_FOR_CLASS_SPEC, SPECIALIZATION, ABILITIES),
+            icon2
+        )
+    end
+end
+
+
+
 function WoWTools_TooltipMixin:Set_Spell(tooltip, spellID)--法术    
     spellID = spellID or select(2, tooltip:GetSpell())
     local name, icon, originalIcon
@@ -16,6 +66,9 @@ function WoWTools_TooltipMixin:Set_Spell(tooltip, spellID)--法术
     local spellTexture=  originalIcon or icon
     tooltip:AddLine(' ')
     tooltip:AddDoubleLine((e.onlyChinese and '法术' or SPELLS)..' '..spellID, spellTexture and '|T'..spellTexture..':0|t'..spellTexture, 1,1,1, 1,1,1)
+
+    Set_HunterPet(tooltip, spellID)--猎人兽栏，宠物
+
     local mountID = spellID~=150544 and C_MountJournal.GetMountFromSpell(spellID)--坐骑
     if mountID then
         WoWTools_TooltipMixin:Set_Mount(tooltip, mountID)
