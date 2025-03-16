0c371eecfe83145851c39e652e28003332e532cb
husandro <husandro@qq.com>
2025-03-16 09:46:54 +0100

    Update Plus_Sort.lua

--- Plus_BankGuild/Plus_Sort.lua
+++ Plus_BankGuild/Plus_Sort.lua
@@ -12,18 +12,22 @@ local function Init(self)
         return
     end
 
-    local saveItemSeconds= Save().saveItemSeconds or 0.8
+    WoWTools_GuildBankMixin.isInRun= true
+
+    local saveItemSeconds= math.min(Save().saveItemSeconds or 1, 1)
     local currentIndex = GetCurrentGuildBankTab() -- 当前 Tab
 
     local itemName, itemLink, itemQuality, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, sellPrice, classID, subclassID, bindType, expansionID, setID, isCraftingReagent
     --local itemID, itemType, itemSubType, itemEquipLoc, icon, classID, subClassID, itemLink
     local items = {}
+
     for slot = 1, MAX_GUILDBANK_SLOTS_PER_TAB do
         itemLink = GetGuildBankItemLink(currentIndex, slot)
         if itemLink then
             itemName, _, itemQuality, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, sellPrice, classID, subclassID, bindType, expansionID, setID, isCraftingReagent= C_Item.GetItemInfo(itemLink)
            -- itemID, itemType, itemSubType, itemEquipLoc, icon, classID, subClassID = C_Item.GetItemInfoInstant(itemLink)
             table.insert(items, {
+
                 slot = slot,
                 link = itemLink,
                 id = C_Item.GetItemInfoInstant(itemLink),
@@ -34,18 +38,36 @@ local function Init(self)
         end
     end
 
+    if #items==0 then
+        self.isInRun= nil
+            WoWTools_GuildBankMixin.isInRun= nil
+            print(
+                WoWTools_GuildBankMixin.addName,
+                '|cffff00ff'..(e.onlyChinese and '尚未发现' or TAXI_PATH_UNREACHABLE)..'|r',
+                e.onlyChinese and '中断' or INTERRUPT
+            )
+        return
+    end
+
     table.sort(items, function(a, b)
         if a.type == b.type then
             if a.subType == b.subType then
                 if a.rarity == b.rarity then
                     return a.id < b.id
+                else
+                    return a.rarity > b.rarity
                 end
-                return a.rarity > b.rarity
+            else
+                return a.subType < b.subType
             end
-            return a.subType < b.subType
+        else
+            return a.type < b.type
         end
-        return a.type < b.type
     end)
+
+    for indexSlot, item in pairs(items) do
+        item.indexSlot= indexSlot
+    end
 
     local find
 
@@ -54,29 +76,41 @@ local function Init(self)
             not self:IsVisible()
             or self.isInRun
             or GetCurrentGuildBankTab()~= currentIndex
-           
+
         then
             self.isInRun= nil
             WoWTools_GuildBankMixin.isInRun= nil
-            print(WoWTools_GuildBankMixin.addName, '|cnRED_FONT_COLOR:'..(e.onlyChinese and '排序' or HUD_EDIT_MODE_SETTING_UNIT_FRAME_SORT_BY)..'|r', e.onlyChinese and '中断' or INTERRUPT  )
+            print(
+                WoWTools_GuildBankMixin.addName,
+                '|cnRED_FONT_COLOR:'..(e.onlyChinese and '排序' or HUD_EDIT_MODE_SETTING_UNIT_FRAME_SORT_BY)..'|r',
+                    e.onlyChinese and '中断' or INTERRUPT
+                )
             return
         end
 
         find=false
-        for i, item in pairs(items) do
-            
-            if item.id and item.slot ~= i then
+        for _, item in pairs(items) do
+
+            if item.slot ~= item.indexSlot and GetGuildBankItemLink(currentIndex, item.indexSlot)~=item.link then
+
                 PickupGuildBankItem(currentIndex, item.slot)
-                PickupGuildBankItem(currentIndex, i)
-               -- items[i]={}
+                PickupGuildBankItem(currentIndex, item.indexSlot)
+
+                item.slot= item.indexSlot
+
                 find=true
-                print(i..')', item.link)
+                print(item.indexSlot..')', item.link)
                 break
             end
         end
 
         if not find then
-            print(WoWTools_GuildBankMixin.addName, '|cnGREEN_FONT_COLOR:'..(e.onlyChinese and '提取' or WITHDRAW)..'|r', e.onlyChinese and '完成' or COMPLETE )
+            WoWTools_GuildBankMixin.isInRun= nil
+            print(
+                WoWTools_GuildBankMixin.addName,
+                '|cnGREEN_FONT_COLOR:'..(e.onlyChinese and '排序' or HUD_EDIT_MODE_SETTING_UNIT_FRAME_SORT_BY)..'|r',
+                e.onlyChinese and '完成' or COMPLETE
+            )
             return
         end
 
@@ -89,8 +123,8 @@ local function Init(self)
 end
 
 
-function WoWTools_GuildBankMixin:Init_Plus_Sort(frame, ...)
+function WoWTools_GuildBankMixin:Init_Plus_Sort(frame)
     if frame then
-        --Init(frame)
+        Init(frame)
     end
 end
