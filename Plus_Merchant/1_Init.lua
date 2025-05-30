01087446d1dae5cd285ea8600317275be3d04f51
husandro <husandro@qq.com>
2025-05-30 18:14:00 +0200

    Update 1_Init.lua

--- Plus_Merchant/1_Init.lua
+++ Plus_Merchant/1_Init.lua
@@ -100,7 +100,7 @@ panel:SetScript("OnEvent", function(self, event, arg1)
                         self:UnregisterEvent('PLAYER_LOGOUT')
                     else
                         self:RegisterEvent("PLAYER_LOGOUT")
-                        Inti()
+                        Init()
                     end
                 end
             })
