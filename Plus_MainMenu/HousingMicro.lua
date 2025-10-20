1efb4d6bf0ccfd9270fad995d23ed08dba77803a
husandro <husandro@qq.com>
2025-10-20 19:00:22 +0200

    1

--- Plus_MainMenu/HousingMicro.lua
+++ Plus_MainMenu/HousingMicro.lua
@@ -1,10 +1,29 @@
+--[[
+function HousingFramesUtil.ToggleHousingDashboard()
+	if (PlayerIsTimerunning() or not C_Housing.IsHousingServiceEnabled()) then
+		return;
+	end
 
+	if not HousingDashboardFrame then
+		C_AddOns.LoadAddOn("Blizzard_HousingDashboard");
+	end
+
+	if (C_PlayerInfo.IsPlayerNPERestricted()) then
+		return;
+	end
+
+	ToggleFrame(HousingDashboardFrame);
+end
+]]
 local function Init()
     if HousingMicroButton then
         HousingMicroButton:HookScript('OnClick', function(_, d)
             if d=='RightButton' and not KeybindFrames_InQuickKeybindMode() and not Kiosk.IsEnabled() then
-                
-
+                HousingFramesUtil.ToggleHousingDashboard()
+                if HousingDashboardFrame:IsShown() then
+                    HousingDashboardFrame.activeTab = HousingDashboardFrame.catalogTab
+                    HousingDashboardFrame:SetTab(HousingDashboardFrame.activeTab)
+                end
             end
         end)
 
