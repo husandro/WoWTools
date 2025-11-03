1ae2961f8340af7a1ea5e7e8d5d669830336ca8a
husandro <husandro@qq.com>
2025-11-03 16:32:31 +0100

    1

---
+++ Plus_Texture/House.lua
@@ -0,0 +1,120 @@
+
+--住房 查找器 11.2.7
+function WoWTools_TextureMixin.Events:Blizzard_HousingBulletinBoard()
+    self:SetButton(HousingBulletinBoardFrame.CloseButton)
+    self:SetAlphaColor(HousingBulletinBoardFrame.GearDropdown.Icon, true)
+    self:SetScrollBar(HousingBulletinBoardFrame.ResidentsTab)
+    self:HideTexture(HousingBulletinBoardFrame.Background)
+    self:SetAlphaColor(HousingBulletinBoardFrame.Footer)
+end
+function WoWTools_TextureMixin.Events:Blizzard_HousingCornerstone()
+    self:SetButton(HousingCornerstoneVisitorFrameCloseButton)
+    self:SetAlphaColor(HousingCornerstoneVisitorFrame.Background)
+    self:SetButton(HousingCornerstoneVisitorFrame.GearDropdown, {alpha=1})
+
+    self:SetButton(HousingCornerstonePurchaseFrameCloseButton)
+    self:SetNineSlice(HousingCornerstonePurchaseFrame.MoneyFrameBackdrop)
+
+    self:SetButton(HousingCornerstoneHouseInfoFrameCloseButton)
+    self:SetAlphaColor(HousingCornerstoneHouseInfoFrame.GearDropdown.Icon, true)
+end
+
+function WoWTools_TextureMixin.Events:Blizzard_HousingDashboard()
+    self:SetButton(HousingDashboardFrameCloseButton)
+    self:SetNineSlice(HousingDashboardFrame)
+    self:SetFrame(HousingDashboardFrame, {alpha=0})
+
+    self:SetAlphaColor(HousingDashboardFrame.HouseInfoTabButton.Icon, true)
+    self:SetAlphaColor(HousingDashboardFrame.CatalogTabButton.Icon, true)
+    self:SetScrollBar(HousingDashboardFrame.CatalogContent.OptionsContainer)
+    HousingDashboardFrame.CatalogContent.TempDisclaimer.DisclaimerText:SetAlpha(0.3)
+
+    self:SetUIButton(HousingDashboardFrame.HouseInfoContent.DashboardNoHousesFrame.NoHouseButton)
+
+    self:SetAlphaColor(HousingDashboardFrame.HouseInfoContent.ContentFrame.HouseUpgradeFrame.TrackFrame.Background, nil, nil, 0.3)
+    self:SetButton(HousingDashboardFrame.HouseInfoContent.ContentFrame.HouseUpgradeFrame.TrackFrame.JumpLeftButton, {alpha=1})
+    self:SetButton(HousingDashboardFrame.HouseInfoContent.ContentFrame.HouseUpgradeFrame.TrackFrame.LeftButton, {alpha=1})
+    self:SetButton(HousingDashboardFrame.HouseInfoContent.ContentFrame.HouseUpgradeFrame.TrackFrame.RightButton, {alpha=1})
+    self:SetButton(HousingDashboardFrame.HouseInfoContent.ContentFrame.HouseUpgradeFrame.TrackFrame.JumpRightButton, {alpha=1})
+    self:SetCheckBox(HousingDashboardFrame.HouseInfoContent.ContentFrame.HouseUpgradeFrame.WatchFavorButton)
+    self:SetButton(HousingDashboardFrame.HouseInfoContent.ContentFrame.HouseUpgradeFrame.TeleportToHouseButton, {alpha=1})
+
+    WoWTools_TextureMixin:Init_BGMenu_Frame(HousingDashboardFrame, {
+        alpha=0.5,
+        nineSliceAlpha=0,
+        portraitAlpha=0.5,
+        settings=function(_, textureName, alphaValue)--设置内容时，调用
+            local alpha= textureName and 0 or alphaValue or 1
+            self:SetAlphaColor(HousingDashboardFrame.CatalogContent.PreviewFrame.PreviewBackground, nil, true, alpha)
+            self:SetAlphaColor(HousingDashboardFrame.CatalogContent.Background, nil, true, alpha)
+            self:SetAlphaColor(HousingDashboardFrame.CatalogContent.Categories.Background, nil, true, alpha)
+            self:SetAlphaColor(HousingDashboardFrame.CatalogContent.Categories.TopBorder, nil, true, alpha)
+            self:SetAlphaColor(HousingDashboardFrame.CatalogContent.Divider, nil, nil, alpha)
+            self:SetAlphaColor(HousingDashboardFrame.HouseInfoContent.ContentFrame.HouseUpgradeFrame.Background, nil, nil, alpha)
+
+            alpha= math.max(alphaValue, 0.5)
+            self:SetAlphaColor(HousingDashboardFrame.HouseInfoContent.DashboardNoHousesFrame.Background, nil, true, alpha)
+        end
+    })
+end
+
+function WoWTools_TextureMixin.Events:Blizzard_HousingCharter()
+    self:SetAlphaColor(HousingCharterFrame.Background)
+end
+
+--住宅区登记表 11.2.7
+function WoWTools_TextureMixin.Events:Blizzard_HousingCreateNeighborhood()
+    self:SetEditBox(HousingCreateNeighborhoodCharterFrame.NeighborhoodNameEditBox)
+    self:SetAlphaColor(HousingCreateNeighborhoodCharterFrame.Background)
+end
+--住宅搜索器
+function WoWTools_TextureMixin.Events:Blizzard_HousingHouseFinder()
+    self:SetButton(HouseFinderFrameCloseButton)
+    self:SetNineSlice(HouseFinderFrame)
+
+    self:SetEditBox(HouseFinderFrame.NeighborhoodListFrame.BNetFriendSearchBox)
+    self:SetButton(HouseFinderFrame.NeighborhoodListFrame.RefreshButton, {alpa=1})
+    self:SetAlphaColor(HouseFinderFrame.NeighborhoodListFrame.NeighborhoodTitleBG, true)
+    self:HideTexture(HouseFinderFrame.NeighborhoodListFrame.NeighborhoodListBG)
+    self:HideFrame(HouseFinderFrame.NeighborhoodListFrame.ListBottomGradient)
+    self:SetScrollBar(HouseFinderFrame.NeighborhoodListFrame.ScrollFrame)
+
+    self:HideTexture(HouseFinderFrame.TopTileStreaks)
+    self:HideTexture(HouseFinderFrame.WoodBorderFrame.Border)
+
+    self:SetUIButton(HouseFinderFrame.PlotInfoFrame.VisitHouseButton)
+    self:SetAlphaColor(HouseFinderFrame.PlotInfoFrame.BackButton.Icon, true)
+    self:SetAlphaColor(HouseFinderFrame.PlotInfoFrame.PlotTitleBG, true)
+    self:HideTexture(HouseFinderFrameBg)
+end
+
+function WoWTools_TextureMixin.Events:Blizzard_HousingHouseSettings()
+    self:SetButton(HousingHouseSettingsFrame.CloseButton)
+    for _, option in pairs(HousingHouseSettingsFrame.PlotAccess.accessOptions or {}) do
+        self:SetCheckBox(option.Checkbox)
+    end
+        for _, option in pairs(HousingHouseSettingsFrame.HouseAccess.accessOptions or {}) do
+        self:SetCheckBox(option.Checkbox)
+    end
+end
+
+--function WoWTools_TextureMixin.Events:Blizzard_HousingControls()
+function WoWTools_TextureMixin.Events:Blizzard_HouseEditor()
+    self:SetScrollBar(HouseEditorFrame.StoragePanel.OptionsContainer)
+    self:HideTexture(HouseEditorFrame.StoragePanel.Categories.Background)
+    self:HideTexture(HouseEditorFrame.StoragePanel.Background)
+    self:HideTexture(HouseEditorFrame.StoragePanel.HeaderBackground)
+
+    self:SetAlphaColor(HouseEditorFrame.StoragePanel.CollapseButton.Icon, nil, nil, 0.5)
+    self:SetAlphaColor(HouseEditorFrame.StorageButton.Icon, nil, nil, 0.5)
+
+    self:HideTexture(HouseEditorFrame.ModeBar.BookendRight)
+    self:HideTexture(HouseEditorFrame.ModeBar.BookendLeft)
+    self:HideTexture(HouseEditorFrame.ModeBar.Background)
+    self:HideTexture(HouseEditorFrame.ModeBar.GradientBackground)
+
+    self:SetButton(HouseEditorFrame.ExteriorCustomizationModeFrame.FixtureOptionList.CloseButton)
+    self:SetScrollBar(HouseEditorFrame.ExteriorCustomizationModeFrame.FixtureOptionList)
+    --self:SetAlphaColor(HouseEditorFrame.ExteriorCustomizationModeFrame.FixtureOptionList.Background)
+    self:SetAlphaColor(HouseEditorFrame.ExteriorCustomizationModeFrame.FixtureOptionList.Header)
+end
