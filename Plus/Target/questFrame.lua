b7f42b118d68c9afefd60d431a9222d55392df7e
husandro <husandro@qq.com>
2026-03-20 17:24:53 +0100

    Update questFrame.lua

--- Plus/Target/questFrame.lua
+++ Plus/Target/questFrame.lua
@@ -5,7 +5,7 @@ local function Save()
     return WoWToolsSave['Plus_Target']
 end
 
-
+--任务，数量
 
 
 
@@ -58,6 +58,8 @@ end
 	self.HealthBarsContainer.healthBar:SetUnit(nil);
 ]]
 ---取得，内容 GameTooltip.lua --local questID= line and line.id
+
+
 local function Get_Unit_Text(self)
     local unit= self.unit
     local isAI= UnitInPartyIsAI(unit)
@@ -116,42 +118,12 @@ end
 
 
 
-
-
---[[
-
---设置，内容
-local function Set_Quest_Text(plate)
-    local frame= plate and plate.UnitFrame
-
-    if not frame then
-        return
-    end
-
-
-    local text, isQuest= Get_Unit_Text(frame.unit)
-
-
-    if not frame.questProgress then
-        frame.questProgress= frame:CreateFontString(nil, 'ARTWORK', 'WoWToolsFonts') -- WoWTools_LabelMixin:Create(frame, {size=14, color={r=0,g=1,b=0}})--14, nil, nil, {0,1,0}, nil,'LEFT')
-        frame.questProgress:SetTextColor(GREEN_FONT_COLOR:GetRGB())
-        frame.questProgress:SetJustifyH('LEFT')
-        --frame.questProgress:SetFontHeight(22)
-        frame.questProgress:SetPoint('LEFT', frame.healthBar or frame, 'RIGHT', 2,0)
-
-    end
-
-    frame.questProgress:SetText(text or '')
-    
-    frame.questProgress:SetFontHeight(frame.isSimplified and 44 or 22)
-    frame.questProgress.isQuest= isQuest
-    if isQuest then
-        frame.HealthBarsContainer.healthBar.selectedBorder:SetVertexColor(1,0,0)
-        frame.HealthBarsContainer.healthBar.selectedBorder:SetShown(true)
-        frame.HealthBarsContainer.healthBar.isTarget=true
-        frame.isSimplified=nil
-    end
-
+local function Create_Label(self)
+    self.questProgress= self:CreateFontString(nil, 'ARTWORK', 'WoWToolsFonts')
+    self.questProgress:SetTextColor(GREEN_FONT_COLOR:GetRGB())
+    self.questProgress:SetJustifyH('LEFT')
+    self.questProgress:SetPoint('LEFT', self.healthBar or self, 'RIGHT', 2, 0)
+    --self.questProgress:SetPoint('LEFT', self.AurasFrame or self, 'RIGHT', 2, 0)
 end
 
 
@@ -159,189 +131,61 @@ end
 
 
 
-
-
-
---检查，所有
-local function Check_AllPlate()
-    for _, plate in pairs(C_NamePlate.GetNamePlates(issecure()) or {}) do
-        --Set_Quest_Text(plate)
-    end
-end
-
-
---移除，内容
-local function RestPlate(plate)
-    if plate and plate.UnitFrame and plate.UnitFrame.questProgress then--任务
-        plate.UnitFrame.questProgress:SetText('')
-    end
-end
---移除，所有内容
-local function RestAllPlate()
-    for _, plate in pairs(C_NamePlate.GetNamePlates(issecure()) or {}) do
-        RestPlate(plate)
-    end
-end]]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---#########
---任务，数量
---#########
 local function Init()
     if not Save().quest then
         return
     end
- --[[
-    questFrame= CreateFrame('Frame')
-
-
-   function questFrame:settings()--注册，事件
-        self:UnregisterAllEvents()
-
-        if not Save().quest then
-            RestAllPlate()
-            return
-        end
-
-        self:RegisterEvent('PLAYER_ENTERING_WORLD')
-
-        --if IsInRaid()
-            --or (IsInInstance() and IsInGroup(LE_PARTY_CATEGORY_HOME) and not WoWTools_MapMixin:IsInDelve())
-            --or WoWTools_MapMixin:IsInPvPArea()--是否在，PVP区域中
-            --or C_ChallengeMode.IsChallengeModeActive()
-        --then
-          --  RestAllPlate()
-            return
-        --end
-
-        FrameUtil.RegisterFrameForEvents(self, {
-            'UNIT_QUEST_LOG_CHANGED',
-            'SCENARIO_UPDATE',
-            'SCENARIO_CRITERIA_UPDATE',
-            'SCENARIO_COMPLETED',
-            'QUEST_POI_UPDATE',
-            'NAME_PLATE_UNIT_ADDED',
-            'GROUP_ROSTER_UPDATE',
-            --'NAME_PLATE_UNIT_REMOVED',
-        })
-
-
-       -- Check_AllPlate()
-    end]]
-
-
-
-
-
-
-
-
-    --[[questFrame:SetScript("OnEvent", function(self, event, arg1)
-        if event=='PLAYER_ENTERING_WORLD' then
-            self:settings()--注册，事件
-
-        elseif event=='NAME_PLATE_UNIT_ADDED' then
-            if arg1 then
-                --Set_Quest_Text(C_NamePlate.GetNamePlateForUnit(arg1, issecure()))--任务
-            end
-
-        elseif event=='GROUP_ROSTER_UPDATE' then
-            Check_AllPlate()
-
-        else--event=='UNIT_QUEST_LOG_CHANGED' or event=='QUEST_POI_UPDATE' or event=='SCENARIO_COMPLETED' or event=='SCENARIO_UPDATE' or event=='SCENARIO_CRITERIA_UPDATE' then
-            C_Timer.After(2, Check_AllPlate)
-        end
-    end)
-
-
-    --questFrame:settings()
-
-
-    WoWTools_DataMixin:Hook(NamePlateBaseMixin, 'ClearUnit', function(plate)--移除所有
-        RestPlate(plate)
-    end)
-]]
+ 
     WoWTools_DataMixin:Hook(NamePlateHealthBarMixin, 'UpdateSelectionBorder', function(self)
-        local questProgress= self:GetParent():GetParent().questProgress
+        if IsInInstance() then
+            return
+        end
+        local unitFrame= self:GetParent()
+        local questProgress= unitFrame and unitFrame.questProgress
         if questProgress and questProgress.isQuest then
             self.selectedBorder:SetVertexColor(1,0,1)
             self.selectedBorder:SetShown(true)
         end
     end)
 
-    --[[hooksecurefunc(NamePlateUnitFrameMixin, 'ShouldBeSimplified', function()
-
-    WoWTools_DataMixin:Hook(NamePlateUnitFrameMixin, 'UpdateIsSimplified', function(self)
-        local questProgress= self:GetParent():GetParent().questProgress
-        if questProgress and canaccessvalue(self.isSimplified) then--and questProgress.isQuest then
-            self.questProgress:SetFontHeight(self.isSimplified and 44 or 22)
-           -- if self.isSimplified then
-                self.isSimplified= false
-                C_NamePlateManager.SetNamePlateSimplified(self.unit, false);
-            --end
-        end
-    end)]]
     WoWTools_DataMixin:Hook(NamePlateUnitFrameMixin, 'OnLoad', function(self)
-        self.questProgress= self:CreateFontString(nil, 'ARTWORK', 'WoWToolsFonts')
+         self.questProgress= self:CreateFontString(nil, 'ARTWORK', 'WoWToolsFonts')
         self.questProgress:SetTextColor(GREEN_FONT_COLOR:GetRGB())
         self.questProgress:SetJustifyH('LEFT')
         self.questProgress:SetPoint('LEFT', self.healthBar or self, 'RIGHT', 2, 0)
-        --self.questProgress:SetPoint('LEFT', self.AurasFrame or self, 'RIGHT', 2, 0)
     end)
 
     WoWTools_DataMixin:Hook(NamePlateUnitFrameMixin, 'OnUnitSet', function(self)
+        if IsInInstance() then
+            return
+        end
         local text, isQuest= Get_Unit_Text(self)
-        self.questProgress:SetText(text or '')
+        if self.questProgress.SetText then
+            self.questProgress:SetText(text)
+        end
+        
         self.questProgress.isQuest= isQuest
         if isQuest then
             self.questProgress:SetFontHeight(canaccessvalue(self.isSimplified) and self.isSimplified and 44 or 22)
             self.HealthBarsContainer.healthBar.selectedBorder:SetVertexColor(1,0,0)
             self.HealthBarsContainer.healthBar.selectedBorder:SetShown(true)
-            --self.HealthBarsContainer.healthBar.isTarget=true
-            --self.isSimplified=nil
-            --self:UpdateIsSimplified()
         end
     end)
 
-    WoWTools_DataMixin:Hook(NamePlateUnitFrameMixin, 'OnUnitCleared', function(self)
+    --[[WoWTools_DataMixin:Hook(NamePlateUnitFrameMixin, 'OnUnitCleared', function(self)
+        if self:HasSecretValues() then
+            print(self.unit, 'UpdateSelectionBorder')
+        end
         self.questProgress:SetText('')
         self.questProgress.isQuest= nil
-    end)
---[[ self:GetScaleData()
-vertical 0.8
-aura 0.75
-horizontal 0.75
-aggroHighlight 1
-classification 0.8
+    end)]]
 
-    WoWTools_DataMixin:Hook(NamePlateUnitFrameMixin, 'UpdateScale', function(self)
-	    local scaleData = self:GetScaleData();
-        info=scaleData
-        for k, v in pairs(info or {}) do if v and type(v)=='table' then print('|cff00ff00---',k, '---STAR|r') for k2,v2 in pairs(v) do print('|cffffff00',k2,v2, '|r') end print('|cffff0000---',k, '---END|r') else print(k,v) end end print('|cffff00ff——————————|r')
-    end)
-]]
     Init=function()
-        --questFrame:settings()
     end
 end
 
 
+        --self.questProgress:SetPoint('LEFT', self.AurasFrame or self, 'RIGHT', 2, 0)
 
 
 
@@ -358,8 +202,33 @@ function WoWTools_TargetMixin:Init_questFrame()
     Init()
 end
 --[[
+self:GetScaleData()
+vertical 0.8
+aura 0.75
+horizontal 0.75
+aggroHighlight 1
+classification 0.8
+
+    WoWTools_DataMixin:Hook(NamePlateUnitFrameMixin, 'UpdateScale', function(self)
+	    local scaleData = self:GetScaleData();
+        info=scaleData
+        for k, v in pairs(info or {}) do if v and type(v)=='table' then print('|cff00ff00---',k, '---STAR|r') for k2,v2 in pairs(v) do print('|cffffff00',k2,v2, '|r') end print('|cffff0000---',k, '---END|r') else print(k,v) end end print('|cffff00ff——————————|r')
+    end)
 
 
+
+[hooksecurefunc(NamePlateUnitFrameMixin, 'ShouldBeSimplified', function()
+
+    WoWTools_DataMixin:Hook(NamePlateUnitFrameMixin, 'UpdateIsSimplified', function(self)
+        local questProgress= self:GetParent():GetParent().questProgress
+        if questProgress and canaccessvalue(self.isSimplified) then--and questProgress.isQuest then
+            self.questProgress:SetFontHeight(self.isSimplified and 44 or 22)
+           -- if self.isSimplified then
+                self.isSimplified= false
+                C_NamePlateManager.SetNamePlateSimplified(self.unit, false);
+            --end
+        end
+    end)
 NamePlateUnitFrameMixin
 SetExplicitValues = function: 000001AA52AE67E8
 IsMinusMob = function: 000001AA52AE6270
